f32e7ca18513c869c3ea37eb308f8eb297b80a11
Jean-Baptiste Assouad <jean.baptiste@assouad.com>
2018-03-08 11:42:02 +0100

    Add button to export csv

--- src/components/waiting-screen.js
+++ src/components/waiting-screen.js
@@ -3,13 +3,35 @@ import { connect } from 'react-redux'
 
 import { selectDatabase } from 'reducers/root-reducer'
 
+class Presentational extends React.Component {
+  constructor(props) {
+    super(props)
+    this.thres = 1000
+    this.last_ms = 0
 
-const Presentational = props => {
-  return (
-    <div>
-      {props.nb_files}
-    </div>
-  )
+    this.shouldComponentUpdate = this.shouldComponentUpdate.bind(this)
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.props.nb_files === nextProps.nb_files) {
+      return false
+    }
+    let cur_ms = (new Date()).getTime()
+    if (cur_ms - this.last_ms < this.thres) {
+      return false
+    }
+    this.last_ms = cur_ms
+    return true
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.props.nb_files}</p>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    )
+  }
 }
 
 
@@ -17,6 +39,7 @@ const mapStateToProps = state => {
   let database = selectDatabase(state)
   return {
     nb_files: database.size()
+    // nb_files: Math.floor(database.size()/1000)*1000
   }
 }
 

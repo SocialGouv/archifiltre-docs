8227b35cb6ed05b2f345f70a78779c94328b5ba7
emmanuelgautier1994 <emmanuel.gre@gmail.com>
2018-04-04 15:01:06 +0200

    first integration of new breadcrumbs, and better folder name display

---
+++ src/button.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const button_style = {
+	width: '80%',
+	margin: '0.2em',
+	fontSize: '0.8em'
+}
+
+export function mkB (click_action, label) {
+	return (
+    <button type="button" onClick={click_action} style={button_style}>
+    	{label}
+    </button>
+  )
+}

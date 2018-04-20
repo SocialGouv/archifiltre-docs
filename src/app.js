8103ad9761fba4dd8fa04491600dcc18682d2d94
Jean-Baptiste Assouad <jean.baptiste@assouad.com>
2018-04-20 15:34:10 +0200

    Start refactoring database.js, implement tree.js

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ import ErrorBoundary from 'components/error-boundary'
 import * as qs from 'query-string'
 import { generateRandomString } from 'random-gen'
 
+import 'tree'
 
 const app = () => {
   let root_div = document.createElement('div')

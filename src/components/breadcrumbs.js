a8b19fc0a8f21c9523077ad2e6b2b1d79a9d71bb
emmanuelgautier1994 <emmanuel.gre@gmail.com>
2018-04-24 19:14:17 +0200

    WIP breadcrumbs

--- src/components/breadcrumbs.js
+++ src/components/breadcrumbs.js
@@ -1,59 +1,82 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import { selectIcicleState } from 'reducers/root-reducer'
+import { selectIcicleState, selectDatabase } from 'reducers/root-reducer'
+
+import { typeOf, icicle_dims } from 'components/icicle'
+
+import BreadCrumbText from 'components/breadcrumb-text'
+import BreadCrumbPoly from 'components/breadcrumb-poly'
 
 import { tr } from 'dict'
 
+const breadcrumb_dims = {w: 400, h: 300}
+
 const Presentational = props => {
-	if(props.hover_sequence.includes(-1)){
-    let res = []
+  let res = []
 
-    for(let i = 0; i < props.hover_sequence.length; i++){
+  if(props.isFocused){
+    for(let i = 1; i < props.hover_sequence.length; i++){
+      let node = props.getByID(props.hover_sequence[i])
+
       res.push(
-        <polygon>
-        </polygon>
-        <text>
-        </text>)
+        <g key={"breadcrumb" + i}>
+          <BreadCrumbPoly
+          // is_last={i === props.hover_sequence.length-1}
+          level={i}
+          step={icicle_dims.h/(props.max_depth+1)}
+          type={typeOf(node)}
+          w={breadcrumb_dims.w}
+          isDummy={false}/>
+          <BreadCrumbText
+          key={"text" + i}
+          text={node.name}
+          level={i}
+          step={icicle_dims.h/(props.max_depth+1)}
+          w={breadcrumb_dims.w}
+          isDummy={false}/>
+        </g>);
     }
-
-    return(
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 300" preserveAspectRatio="xMidYMid meet">
-        <g>
-          <polygon></polygon>
-          <text>
-          </text>
-        </g>
-      </svg>
-      );
   }
   else{
-    let res = []
-
-    for(let i = 0; i < props.hover_sequence.length; i++){
+    for(let i = 1; i < props.max_depth+1; i++){
       res.push(
-        <polygon>
-        </polygon>
-        <text>
-        </text>)
+        <g key={"breadcrumb" + i}>
+          <BreadCrumbPoly
+          // is_last={i === props.max_depth}
+          level={i}
+          step={icicle_dims.h/(props.max_depth+1)}
+          type={typeOf({children:[], name:''})}
+          w={breadcrumb_dims.w}
+          isDummy={true}/>
+          <BreadCrumbText
+          key={"text" + i}
+          text={(i === props.max_depth ? tr("File") : tr("Level") + " " + (i))}
+          level={i}
+          step={icicle_dims.h/(props.max_depth+1)}
+          w={breadcrumb_dims.w}
+          isDummy={true}/>
+        </g>);
     }
-    
-    return (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 300" preserveAspectRatio="xMidYMid meet">
-        <g>
-          {res}
-        </g>
-      </svg>
-        );
   }
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 300" preserveAspectRatio="xMidYMid meet" style={{"opacity": (props.isFocused ? 1 : 0.3)}}>
+      {res}
+    </svg>
+      );
 
 }
 
 
 const mapStateToProps = state => {
 	let icicle_state = selectIcicleState(state)
+  let database = selectDatabase(state)
 	return {
-		hover_sequence: icicle_state.hover_sequence()
+		hover_sequence: icicle_state.hover_sequence(),
+    isFocused: icicle_state.isFocused(),
+    max_depth: database.max_depth(),
+    getByID : database.getByID
 	}
 }
 

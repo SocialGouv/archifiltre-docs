d510e4fdd984a4ba0f0346767657c4699e99e486
emmanuelgautier1994 <emmanuel.gre@gmail.com>
2018-04-24 16:32:49 +0200

    functional ruler

--- src/components/icicle-rect.js
+++ src/components/icicle-rect.js
@@ -5,23 +5,27 @@ import { selectIcicleState } from 'reducers/root-reducer'
 
 import { setFocus } from 'reducers/icicle-state'
 
+import { typeOf } from 'components/icicle'
+
 import { tr } from 'dict'
 
 const Presentational = props => {
 	let node = props.node
+	let node_dims = props.dims
+	let node_id = props.node_id
 
-	let opacity = props.hover_sequence.includes(-1) ? 1 : (props.hover_sequence.includes(node.id) ? 1 : 0.3)
+	let opacity = props.hover_sequence.includes(-1) ? 1 : (props.hover_sequence.includes(node_id) ? 1 : 0.3)
 	let display = node.depth ? "" : "none"
 
   return (<rect
       className="node"
-      x={node.x}
-      y={node.y}
-      width={node.dx}
-      height={node.dy}
+      x={node_dims.x}
+      y={node_dims.y}
+      width={node_dims.dx}
+      height={node_dims.dy}
       onClick={(e) => {e.stopPropagation()}}
-      onMouseOver={() => {props.setFocus(props.node_sequence)}}
-      style={{"fill": props.type.color, "opacity": opacity, "display" : display}}></rect>);
+      onMouseOver={() => {props.setFocus(props.node_sequence, node_dims)}}
+      style={{"fill": typeOf(node).color, "opacity": opacity, "display" : display}}></rect>);
 }
 
 

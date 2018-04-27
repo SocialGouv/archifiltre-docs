224f002e32368e65d008b687801d43a9ed3504a4
emmanuelgautier1994 <emmanuel.gre@gmail.com>
2018-04-27 10:46:41 +0200

    Now fully implemented zooming and dezooming

--- src/components/icicle-rect.js
+++ src/components/icicle-rect.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 
 import { selectIcicleState } from 'reducers/root-reducer'
 
-import { setFocus, setDisplayRoot } from 'reducers/icicle-state'
+import { setFocus, setDisplayRoot, setNoDisplayRoot } from 'reducers/icicle-state'
 
 import { typeOf } from 'components/icicle'
 
@@ -17,6 +17,8 @@ const Presentational = props => {
 	let opacity = props.hover_sequence.includes(-1) ? 1 : (props.hover_sequence.includes(node_id) ? 1 : 0.3)
 	let display = node.depth ? "" : "none"
 
+  let is_parent = props.isZoomed && props.display_root.includes(node_id) && node.children.length
+
   let res = [(<rect
       key="rect"
       className="node"
@@ -26,16 +28,16 @@ const Presentational = props => {
       height={node_dims.dy}
       onClick={(e) => {e.stopPropagation(); props.setDisplayRoot(props.node_sequence)}}
       onMouseOver={() => {props.setFocus(props.node_sequence, node_dims)}}
-      style={{"fill": typeOf(node).color, "opacity": opacity, "display" : display}}></rect>)]
+      style={{"fill": is_parent ? typeOf({children:["-1"], name:''}).color : typeOf(node).color, "opacity": opacity, "display" : display}}></rect>)]
 
-  if(!(node.depth)) res.push(<text
+  if(!(node.depth) && props.isZoomed) res.push(<text
     x={node_dims.dx/2}
     y={node_dims.dy/2}
     dx="0"
     dy={node_dims.dy/4}
     textAnchor="middle"
     stroke="none"
-    onClick={(e) => {e.stopPropagation(); if(props.isZoomed) props.setDisplayRoot(node_id) ;}}
+    onClick={(e) => {e.stopPropagation(); props.setNoDisplayRoot() ;}}
     key="text">{tr("Back to root")}</text>)
 
   return res;
@@ -46,6 +48,7 @@ const mapStateToProps = state => {
 	let icicle_state = selectIcicleState(state)
 	return {
 		hover_sequence: icicle_state.hover_sequence(),
+    display_root: icicle_state.display_root(),
     isZoomed: icicle_state.isZoomed()
 	}
 }
@@ -53,7 +56,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
  	return {
     setFocus: (...args) => dispatch((setFocus(...args))),
- 		setDisplayRoot: (...args) => dispatch((setDisplayRoot(...args)))
+    setDisplayRoot: (...args) => dispatch((setDisplayRoot(...args))),
+ 		setNoDisplayRoot: (...args) => dispatch((setNoDisplayRoot(...args)))
  	}
 }
 

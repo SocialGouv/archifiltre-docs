03853d109735d6ae62a20e8ae333e271ed4d9cea
emmanuelgautier1994 <emmanuel.gre@gmail.com>
2018-05-03 17:46:37 +0200

    Clicking a node now freezes the icicle, in order for you to go and edit items on the report. Need to implement zooming as a double-click feature now.

--- src/reducers/icicle-state.js
+++ src/reducers/icicle-state.js
@@ -7,6 +7,7 @@ const type = 'cheapExp/icicleState'
 
 const State = Record({
   hover_seq:[-1],
+  lock_seq:[-1],
   dims: {},
   display_root:[-1]
 })
@@ -14,9 +15,11 @@ const State = Record({
 function bundle(state) {
   return {
     hover_sequence: () => state.get('hover_seq'),
+    lock_sequence: () => state.get('lock_seq'),
     hover_dims: () => state.get('dims'),
-    isFocused: () => !(state.get('hover_seq').includes(-1)),
     display_root: () => state.get('display_root'),
+    isFocused: () => !(state.get('hover_seq').includes(-1)),
+    isLocked: () => !(state.get('lock_seq').includes(-1)),
     isZoomed: () => !(state.get('display_root').includes(-1))
   }
 }
@@ -28,15 +31,26 @@ const { mkA, reducer } = duck(type, initialState, bundle)
 
 export default reducer
 
-export const setFocus = mkA((id_arr, dims) => state => {
+export const setFocus = mkA((id_arr, dims, isLocked) => state => {
   state = state.update('hover_seq',()=>id_arr)
-  state = state.update('dims',()=>dims)
+  if(!isLocked) state = state.update('dims',()=>dims);
   return state
 })
 
 export const setNoFocus = mkA(() => state => {
   state = state.update('hover_seq', () => [-1])
   state = state.update('dims',()=>{})
+  return state
+})
+
+export const lock = mkA((id_arr, dims) => state => {
+  state = state.update('lock_seq',()=>id_arr)
+  state = state.update('dims',()=>dims);
+  return state
+})
+
+export const unlock = mkA(() => state => {
+  state = state.update('lock_seq', () => [-1])
   return state
 })
 

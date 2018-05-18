6c44f5be3fd8da9539b3b110226a8be84d31e8f7
emmanuelgautier1994 <emmanuel.gre@gmail.com>
2018-05-18 14:19:54 +0200

    Further styling, and new back to root button

---
+++ src/components/back-to-root-button.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import { mkB } from 'components/button'
+
+import { setNoDisplayRoot } from 'reducers/icicle-state'
+import { setNoFocus } from 'reducers/icicle-state'
+
+import { selectIcicleState, commit } from 'reducers/root-reducer'
+
+import { types } from 'components/icicle'
+
+import { tr } from 'dict'
+
+const Presentational = props => {
+
+  let div_style = {
+    opacity: props.isZoomed ? '1' : '0',
+    transition : 'opacity 0.2s ease-out',
+    WebkitTransition : 'opacity 0.2s ease-out',
+  }
+
+  return <div style={div_style}>{mkB(props.backToRoot, tr("Back to root"), true, types.parent_folder.color)}</div>
+}
+
+
+
+const mapStateToProps = state => {
+  let icicle_state = selectIcicleState(state)
+
+  return {
+    isZoomed: icicle_state.isZoomed(),
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    backToRoot: (...args) => {
+      dispatch(setNoDisplayRoot())
+      dispatch(setNoFocus())
+      dispatch(commit())
+    }
+  }
+}
+
+
+const Container = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Presentational)
+
+export default Container
--- src/components/workspace.js
+++ src/components/workspace.js
@@ -11,10 +11,15 @@ import Icicle from 'components/icicle'
 import Ruler from 'components/ruler'
 import BreadCrumbs from 'components/breadcrumbs'
 import Report from 'components/report'
-
+import BTRButton from 'components/back-to-root-button'
 
 const chart_style = {
   stroke: '#fff',
+}
+
+const btr_style = {
+  textAlign: 'center',
+  paddingTop: '2em'
 }
 
 
@@ -22,7 +27,14 @@ const Presentational = props => {
   return (
     <div>
         <Report />
-        <div className="grid-x grid-frame" style={{marginTop: '1em'}}>
+        <div className="grid-x grid-frame">
+          <div className="cell small-2"></div>
+          <div className="cell small-4" style={btr_style}>
+            <BTRButton />
+          </div>
+          <div className="cell small-6"></div>
+        </div>
+        <div className="grid-x grid-frame">
           <div
             onClick={(e) => {props.unlock(); props.setNoFocus();}}
             className="cell small-8"

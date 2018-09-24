9516420a20c8e6eb68ee79c1a0a19e78c05e9baf
Jean-Baptiste Assouad <jean.baptiste@assouad.com>
2018-09-24 18:53:53 +0200

    refactoring all_tags, delete tag_list_state (useless)

--- src/components/all-tags.js
+++ src/components/all-tags.js
@@ -34,123 +34,173 @@ const content_style = {
 }
 
 
-const AllTags = props => {
-  const tag_ids = props.tag_ids
+class AllTags extends React.Component {
+  constructor(props) {
+    super(props)
 
-  const component_style = {
-    'opacity': (tag_ids.size > 0 ? 1 : 0.5),
-    'background': 'white',
-    'height': '100%',
-    'borderRadius': '1em',
-    padding:'0.5em 0'
+    this.state = {
+      editing_tag_id:'',
+    }
+
+    this.startEditingTagFactory = this.startEditingTagFactory.bind(this)
+    this.stopEditingTag = this.stopEditingTag.bind(this)
   }
 
-  let tags_content
+  startEditingTagFactory(editing_tag_id) {
+    return () => this.setState({
+      editing_tag_id,
+    })
+  }
 
-  const getTagByTagId = props.getTagByTagId
-  const tag_id_being_edited = props.tag_id_being_edited
-  const tag_id_to_highlight = props.tag_id_to_highlight
+  stopEditingTag() {
+    this.setState({
+      editing_tag_id:'',
+    })
+  }
 
-  const total_volume = props.total_volume
-  const focused_node_id = props.focused_node_id
+  render() {
+    const props = this.props
 
-  const computeOpacity = (tag_id) => {
-    if (tag_id_being_edited.length > 0) {
-      if (tag_id_being_edited === tag_id) {
-        return 1
-      } else {
-        return 0.2
-      }
-    } else {
-      if (tag_id_to_highlight.length > 0) {
-        if (tag_id_to_highlight === tag_id) {
+    const getTagByTagId = props.getTagByTagId
+    const tag_id_to_highlight = props.tag_id_to_highlight
+    const total_volume = props.total_volume
+    const focused_node_id = props.focused_node_id
+    const tag_ids = props.tag_ids
+    
+    const state = this.state
+
+    const editing_tag_id = state.editing_tag_id
+
+
+    const startEditingTagFactory = this.startEditingTagFactory
+    const stopEditingTag = this.stopEditingTag
+
+
+    const component_style = {
+      'opacity': (tag_ids.size > 0 ? 1 : 0.5),
+      'background': 'white',
+      'height': '100%',
+      'borderRadius': '1em',
+      padding:'0.5em 0'
+    }
+
+    let tags_content
+
+
+    const computeOpacity = (tag_id) => {
+      if (editing_tag_id.length > 0) {
+        if (editing_tag_id === tag_id) {
           return 1
         } else {
           return 0.2
         }
       } else {
-        return 1
+        if (tag_id_to_highlight.length > 0) {
+          if (tag_id_to_highlight === tag_id) {
+            return 1
+          } else {
+            return 0.2
+          }
+        } else {
+          return 1
+        }
       }
     }
-  }
 
-  // Dummy display for when there aren't any tags yet
-  if (tag_ids.size === 0) {
-    tags_content = (
-      <div className='grid-y grid-frame align-center' style={{height:'75%'}}>
-        <div className='cell'>
-          <TextAlignCenter>
-            <i className='fi-pricetag-multiple' style={{
-              'color': Color.placeholder(),
-              'fontSize': '4em',
-              'lineHeight': 0}}
-            />
-          </TextAlignCenter>
+    // Dummy display for when there aren't any tags yet
+    if (tag_ids.size === 0) {
+      tags_content = (
+        <div className='grid-y grid-frame align-center' style={{height:'75%'}}>
+          <div className='cell'>
+            <TextAlignCenter>
+              <i className='fi-pricetag-multiple' style={{
+                'color': Color.placeholder(),
+                'fontSize': '4em',
+                'lineHeight': 0}}
+              />
+            </TextAlignCenter>
+          </div>
+          <div className='cell'>
+            <TextAlignCenter>
+              <em>{no_tags}</em>
+            </TextAlignCenter>
+          </div>
         </div>
-        <div className='cell'>
-          <TextAlignCenter>
-            <em>{no_tags}</em>
-          </TextAlignCenter>
-        </div>
-      </div>
-    )
-  } else {
-    // Displaying the list of all tags
-
-    const tags_list = tag_ids.map(tag_id => {
-      const tag = getTagByTagId(tag_id)
-      const name = tag.get('name')
-      const ff_ids = tag.get('ff_ids')
-      const size = tag.get('size')
-
-      const opacity = computeOpacity(tag_id)
-      
-      const percentage = Math.floor((size / total_volume)*100)
-
-      const editing = tag_id_being_edited === tag_id
-      const shoud_display_count = (focused_node_id === undefined)
-      const node_has_tag = ff_ids.has(focused_node_id)
-
-      return (
-        <TagListItem
-          key={tag_id}
-          tag={name}
-          opacity={opacity}
-          editing={editing}
-          percentage={percentage}
-          shoud_display_count={shoud_display_count}
-          node_has_tag={node_has_tag}
-          tag_number={ff_ids.size}
-          highlightTag={props.highlightTag(tag_id)}
-          stopHighlightingTag={props.stopHighlightingTag}
-          startEditingTag={props.startEditingTag(tag_id)}
-          stopEditingTag={props.stopEditingTag}
-          deleteTag={props.onDeleteTag(tag_id)}
-          renameTag={props.onRenameTag(tag_id)}
-          addTagToNode={props.onAddTagged(focused_node_id,tag_id)}
-          removeTagFromNode={props.onDeleteTagged(focused_node_id,tag_id)}
-        />
       )
-    }).reduce((acc,val)=>[...acc,val],[])
+    } else {
+      // Displaying the list of all tags
 
 
-    tags_content = (
-      <div style={content_style} onMouseLeave={props.stopHighlightingTag}>
-        {tags_list}
+      const comparator = (a,b) => {
+        a = getTagByTagId(a).get('name')
+        b = getTagByTagId(b).get('name')
+        if (a < b) {
+          return -1
+        } else if (a === b) {
+          return 0
+        } else {
+          return 1
+        }
+      }
+
+      const tags_list = tag_ids
+        .sort(comparator)
+        .map(tag_id => {
+          const tag = getTagByTagId(tag_id)
+          const name = tag.get('name')
+          const ff_ids = tag.get('ff_ids')
+          const size = tag.get('size')
+
+          const opacity = computeOpacity(tag_id)
+          
+          const percentage = Math.floor((size / total_volume)*100)
+
+          const editing = editing_tag_id === tag_id
+          const shoud_display_count = (focused_node_id === undefined)
+          const node_has_tag = ff_ids.has(focused_node_id)
+
+          return (
+            <TagListItem
+              key={tag_id}
+              tag={name}
+              opacity={opacity}
+              editing={editing}
+              percentage={percentage}
+              shoud_display_count={shoud_display_count}
+              node_has_tag={node_has_tag}
+              tag_number={ff_ids.size}
+              highlightTag={props.highlightTag(tag_id)}
+              stopHighlightingTag={props.stopHighlightingTag}
+              startEditingTag={startEditingTagFactory(tag_id)}
+              stopEditingTag={stopEditingTag}
+              deleteTag={props.onDeleteTag(tag_id)}
+              renameTag={props.onRenameTag(tag_id)}
+              addTagToNode={props.onAddTagged(focused_node_id,tag_id)}
+              removeTagFromNode={props.onDeleteTagged(focused_node_id,tag_id)}
+            />
+          )
+        })
+        .reduce((acc,val)=>[...acc,val],[])
+
+
+      tags_content = (
+        <div style={content_style} onMouseLeave={props.stopHighlightingTag}>
+          {tags_list}
+        </div>
+      )
+    }
+
+
+
+    return (
+      <div style={component_style}>
+        <TextAlignCenter>
+          <b>{all_tags}</b>
+        </TextAlignCenter>
+          {tags_content}
       </div>
     )
   }
-
-
-
-  return (
-    <div style={component_style}>
-      <TextAlignCenter>
-        <b>{all_tags}</b>
-      </TextAlignCenter>
-        {tags_content}
-    </div>
-  )
 }
 
 
@@ -159,11 +209,8 @@ const AllTags = props => {
 export default (props) => {
   const api = props.api
   const icicle_state = api.icicle_state
-  const tag_list_state = api.tag_list_state
   const database = api.database
 
-  // const tags = database.getAllTags().sortBy(t => -1 * t.size)
-  // const tags_sizes = database.getAllTagsSizes()
   const tag_ids = database.getAllTagIds()
   
   const total_volume = database.volume()
@@ -173,7 +220,6 @@ export default (props) => {
 
   const tag_id_to_highlight = icicle_state.tagIdToHighlight()
 
-  const tag_id_being_edited = tag_list_state.tagIdBeingEdited()
 
 
 
@@ -183,14 +229,6 @@ export default (props) => {
 
   const stopHighlightingTag = () => {
     icicle_state.setNoTagIdToHighlight()
-  }
-
-  const startEditingTag = (tag_id) => () => {
-    tag_list_state.setTagIdBeingEdited(tag_id)
-  }
-
-  const stopEditingTag = () => {
-    tag_list_state.setNoTagIdBeingEdited()
   }
 
   const onRenameTag = (tag_id) => (name) => {
@@ -223,15 +261,12 @@ export default (props) => {
     tag_ids,
     tag_id_to_highlight,
     focused_node_id,
-    tag_id_being_edited,
     total_volume,
     getTagByTagId:database.getTagByTagId,
 
 
     highlightTag,
     stopHighlightingTag,
-    startEditingTag,
-    stopEditingTag,
     onRenameTag,
     onDeleteTag,
     onAddTagged,

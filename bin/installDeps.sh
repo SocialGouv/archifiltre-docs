374a09c399d07b008d60d4bfeeff51db50c6890b
Jean-Baptiste Assouad <jean.baptiste@assouad.com>
2018-10-08 12:21:47 +0200

    change make name target for dependencies

--- bin/installDeps.sh
+++ bin/installDeps.sh
@@ -67,6 +67,7 @@ sudo dpkg --add-architecture i386
 
 wget -nc https://dl.winehq.org/wine-builds/Release.key
 sudo apt-key add Release.key
+rm Release.key
 sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
 
 sudo apt-get update && sudo apt-get -y install --install-recommends winehq-stable

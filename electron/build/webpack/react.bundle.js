3e1d5930becca4592d7d38520fa04b8c8e72e2f7
emmanuelgautier1994 <emmanuel.gre@gmail.com>
2018-10-10 16:31:07 +0200

    Tweaked header for font family and size

---
+++ electron/build/webpack/react.bundle.js
@@ -0,0 +1,12 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["react"],[
+/* 0 */
+/*!*****************************!*\
+  !*** multi react react-dom ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("__webpack_require__(/*! react */\"./node_modules/react/index.js\");\nmodule.exports = __webpack_require__(/*! react-dom */\"./node_modules/react-dom/index.js\");\n\n\n//# sourceURL=webpack:///multi_react_react-dom?");
+
+/***/ })
+],[[0,"runtime~react","vendors~app~react~redux","vendors~app~react"]]]);

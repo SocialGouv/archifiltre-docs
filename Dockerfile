38fe16f380abc324f9036b9715eda0967a70c3f0
Jean-Baptiste Assouad <jean.baptiste@assouad.com>
2019-04-23 15:11:48 +0200

    some clean up

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,4 @@
-FROM node:9.7.1 as dev
-
-RUN apt-get update && apt-get -y install \
-  inotify-tools=3.14-1+b1 \
-  rsync=3.1.1-3+deb8u1
-
-
-WORKDIR /usr/src/app/patch
-COPY patch .
-Run make fetchAndPatch
-
-
+FROM node as dev
 
 WORKDIR /usr/src/app
 COPY package*.json ./
--- src/compatibility.js
+++ src/compatibility.js
@@ -3,10 +3,12 @@ import * as Loop from 'test/loop'
 
 import { generateRandomString } from 'random-gen'
 
-import * as V5 from '../version/v5/src/file-system'
-import * as V6 from '../version/v6/src/file-system'
-import * as V7 from '../version/v7/src/file-system'
-import * as V8 from '../version/v8/src/file-system'
+// DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+// import * as V5 from '../version/v5/src/file-system'
+// import * as V6 from '../version/v6/src/file-system'
+// import * as V7 from '../version/v7/src/file-system'
+// import * as V8 from '../version/v8/src/file-system'
+// DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
 
 export const fromAnyJsonToJs = (json) => {
   const version = JSON.parse(json).version
@@ -14,26 +16,28 @@ export const fromAnyJsonToJs = (json) => {
   let js
 
   switch (version) {
-    case 5:
-      if (js===undefined) {
-        js = V5.toJs(V5.fromJson(json))
-      }
-      js = v5JsToV6Js(js)
-    case 6:
-      if (js===undefined) {
-        js = V6.toJs(V6.fromJson(json))
-      }
-      js = v6JsToV7Js(js)
-    case 7:
-      if (js===undefined) {
-        js = V7.toJs(V7.fromJson(json))
-      }
-      js = v7JsToV8Js(js)
-    case 8:
-      if (js===undefined) {
-        js = V8.toJs(V8.fromJson(json))
-      }
-      js = v8JsToV9Js(js)
+    // DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+    // case 5:
+    //   if (js===undefined) {
+    //     js = V5.toJs(V5.fromJson(json))
+    //   }
+    //   js = v5JsToV6Js(js)
+    // case 6:
+    //   if (js===undefined) {
+    //     js = V6.toJs(V6.fromJson(json))
+    //   }
+    //   js = v6JsToV7Js(js)
+    // case 7:
+    //   if (js===undefined) {
+    //     js = V7.toJs(V7.fromJson(json))
+    //   }
+    //   js = v7JsToV8Js(js)
+    // case 8:
+    //   if (js===undefined) {
+    //     js = V8.toJs(V8.fromJson(json))
+    //   }
+    //   js = v8JsToV9Js(js)
+    // DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
     case 9:
       if (js===undefined) {
         js = JSON.parse(json)
@@ -84,278 +88,279 @@ const unzip3 = l => {
 }
 
 
-export const v5JsToV6Js = (v5) => {
-  const convFs = a => {
-    a = Object.assign({},a)
-    a.version = 6
-    delete a.nb_push
-    a.content_queue = a.content_queue.map(convCq)
-    const tags = {}
-    a.tree = convTT(tags,a.tree)
-    a.tags = tags
-    return a
-  }
+// DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+// export const v5JsToV6Js = (v5) => {
+//   const convFs = a => {
+//     a = Object.assign({},a)
+//     a.version = 6
+//     delete a.nb_push
+//     a.content_queue = a.content_queue.map(convCq)
+//     const tags = {}
+//     a.tree = convTT(tags,a.tree)
+//     a.tags = tags
+//     return a
+//   }
 
-  const convCq = a => {
-    a = Object.assign({},a)
-    a.content = convContent(a.content)
-    return a
-  }
+//   const convCq = a => {
+//     a = Object.assign({},a)
+//     a.content = convContent(a.content)
+//     return a
+//   }
 
-  const convContent = a => {
-    a = Object.assign({},a)
-    delete a.error_is_file
-    a.last_modified = {
-      max:a.last_modified,
-      list:[a.last_modified],
-      min:a.last_modified,
-      median:a.last_modified,
-      average:a.last_modified,
-    }
-    a.nb_files = 1
-    return a
-  }
-
-
-  const convTT = (tags,a) => {
-    a = Object.assign({},a)
-    a.table = Object.assign({},a.table)
-
-    const reduceSum = arr => arr.reduce((acc,val)=>acc+val,0)
-    const reduceLM = arr => {
-      const a = {
-        list:arr.reduce((acc,val)=>acc.concat(val.list), [])
-      }
-      a.max = max(0,a.list)
-      a.min = min(Number.MAX_SAFE_INTEGER,a.list)
-      a.median = median(a.list)
-      a.average = average(a.list)
-      return a
-    }
+//   const convContent = a => {
+//     a = Object.assign({},a)
+//     delete a.error_is_file
+//     a.last_modified = {
+//       max:a.last_modified,
+//       list:[a.last_modified],
+//       min:a.last_modified,
+//       median:a.last_modified,
+//       average:a.last_modified,
+//     }
+//     a.nb_files = 1
+//     return a
+//   }
 
 
-    const addTag = (id) => (tag) => {
-      if (tags[tag]) {
-        if (tags[tag].includes(id) === false) {
-          tags[tag].push(id)
-        }
-      } else {
-        tags[tag] = [id]
-      }
-    }
+//   const convTT = (tags,a) => {
+//     a = Object.assign({},a)
+//     a.table = Object.assign({},a.table)
 
-    const convEntry = (id) => {
-      const entry = Object.assign({},a.table[id])
-      entry.content = convContent(entry.content)
-      entry.max_children_path_length = 0
-      entry.sum_children_path_length = 0
-      entry.parent_path_length = 0
-      entry.content.tags.forEach(addTag(id))
+//     const reduceSum = arr => arr.reduce((acc,val)=>acc+val,0)
+//     const reduceLM = arr => {
+//       const a = {
+//         list:arr.reduce((acc,val)=>acc.concat(val.list), [])
+//       }
+//       a.max = max(0,a.list)
+//       a.min = min(Number.MAX_SAFE_INTEGER,a.list)
+//       a.median = median(a.list)
+//       a.average = average(a.list)
+//       return a
+//     }
 
-      const array = unzip3(entry.children.map(convEntry))
+
+//     const addTag = (id) => (tag) => {
+//       if (tags[tag]) {
+//         if (tags[tag].includes(id) === false) {
+//           tags[tag].push(id)
+//         }
+//       } else {
+//         tags[tag] = [id]
+//       }
+//     }
+
+//     const convEntry = (id) => {
+//       const entry = Object.assign({},a.table[id])
+//       entry.content = convContent(entry.content)
+//       entry.max_children_path_length = 0
+//       entry.sum_children_path_length = 0
+//       entry.parent_path_length = 0
+//       entry.content.tags.forEach(addTag(id))
+
+//       const array = unzip3(entry.children.map(convEntry))
       
-      if (array[0].length) {
-        entry.content.size = sum(array[0])
-        entry.content.last_modified = reduceLM(array[1])
-        entry.content.nb_files = sum(array[2])
-      }
+//       if (array[0].length) {
+//         entry.content.size = sum(array[0])
+//         entry.content.last_modified = reduceLM(array[1])
+//         entry.content.nb_files = sum(array[2])
+//       }
 
-      a.table[id] = entry
+//       a.table[id] = entry
 
-      return [
-        entry.content.size,
-        entry.content.last_modified,
-        entry.content.nb_files,
-      ]
-    }
+//       return [
+//         entry.content.size,
+//         entry.content.last_modified,
+//         entry.content.nb_files,
+//       ]
+//     }
 
-    convEntry(a.root_id)
-    return a
-  }
+//     convEntry(a.root_id)
+//     return a
+//   }
 
-  return convFs(v5)
-}
-
-
+//   return convFs(v5)
+// }
 
 
 
 
-export const v6JsToV7Js = (v6) => {
-  const v7 = Object.assign({},v6)
-  v7.version = 7
-  return v7
-}
+
+
+// export const v6JsToV7Js = (v6) => {
+//   const v7 = Object.assign({},v6)
+//   v7.version = 7
+//   return v7
+// }
 
 
 
-export const v7JsToV8Js = (v7) => {
-  const v8 = Object.assign({},v7)
-  v8.version = 8
-  v8.tags_sizes = {}
+// export const v7JsToV8Js = (v7) => {
+//   const v8 = Object.assign({},v7)
+//   v8.version = 8
+//   v8.tags_sizes = {}
 
-  const table = v8.tree.table
+//   const table = v8.tree.table
 
 
-  const sortBySize = (ids) => {
-    const compare = (a,b) => {
-      const s_a = table[a].content.size
-      const s_b = table[b].content.size
-      if (s_a > s_b) {
-        return -1
-      } else if (s_a === s_b) {
-        return 0
-      } else {
-        return 1
-      }
-    }
-    const sizes = ids.sort(compare)
-    return sizes
-  }
+//   const sortBySize = (ids) => {
+//     const compare = (a,b) => {
+//       const s_a = table[a].content.size
+//       const s_b = table[b].content.size
+//       if (s_a > s_b) {
+//         return -1
+//       } else if (s_a === s_b) {
+//         return 0
+//       } else {
+//         return 1
+//       }
+//     }
+//     const sizes = ids.sort(compare)
+//     return sizes
+//   }
 
-  const filterChildren = (ids) => {
-    const getAllChildren = (id) => {
-      const children = table[id].children
-      return children.concat(children.map(getAllChildren).reduce((acc,val)=>acc.concat(val),[]))
-    }
+//   const filterChildren = (ids) => {
+//     const getAllChildren = (id) => {
+//       const children = table[id].children
+//       return children.concat(children.map(getAllChildren).reduce((acc,val)=>acc.concat(val),[]))
+//     }
 
-    if (ids.length <= 1) {
-      return ids
-    } else {
-      const head_id = ids[0]
-      const children_head_id = getAllChildren(head_id)
+//     if (ids.length <= 1) {
+//       return ids
+//     } else {
+//       const head_id = ids[0]
+//       const children_head_id = getAllChildren(head_id)
 
-      const tail = ids.slice(1)
-      const filtered_tail = tail.filter(a=>children_head_id.includes(a)===false)
-      return [head_id].concat(filterChildren(filtered_tail))
-    }
-  }
+//       const tail = ids.slice(1)
+//       const filtered_tail = tail.filter(a=>children_head_id.includes(a)===false)
+//       return [head_id].concat(filterChildren(filtered_tail))
+//     }
+//   }
 
-  const reduceToSize = (ids) => {
-    return ids.reduce((acc,val)=>acc+table[val].content.size,0)
-  }
+//   const reduceToSize = (ids) => {
+//     return ids.reduce((acc,val)=>acc+table[val].content.size,0)
+//   }
   
-  for (let key in table) {
-    delete table[key].content.tags
-  }
+//   for (let key in table) {
+//     delete table[key].content.tags
+//   }
 
-  for (let key in v8.content_queue) {
-    delete v8.content_queue[key].content.tags
-  }
+//   for (let key in v8.content_queue) {
+//     delete v8.content_queue[key].content.tags
+//   }
 
-  for (let key in v8.tags) {
-    v8.tags_sizes[key] = reduceToSize(filterChildren(sortBySize(v8.tags[key])))
-  }
+//   for (let key in v8.tags) {
+//     v8.tags_sizes[key] = reduceToSize(filterChildren(sortBySize(v8.tags[key])))
+//   }
 
-  return v8
-}
-
-
+//   return v8
+// }
 
 
 
 
 
-export const v8JsToV9Js = (v8) => {
-  const v9 = Object.assign({},v8)
-  v9.version = 9
 
-  delete v9.content_queue
-  delete v9.tree
-  delete v9.tags
-  delete v9.tags_sizes
-  delete v9.parent_path
 
-  const mapOldToNewId = {}
+// export const v8JsToV9Js = (v8) => {
+//   const v9 = Object.assign({},v8)
+//   v9.version = 9
+
+//   delete v9.content_queue
+//   delete v9.tree
+//   delete v9.tags
+//   delete v9.tags_sizes
+//   delete v9.parent_path
+
+//   const mapOldToNewId = {}
   
 
-  const v8TreeToV9Ffs = tree => {
-    const table = tree.table
+//   const v8TreeToV9Ffs = tree => {
+//     const table = tree.table
 
-    const remakePath = (key,table) => {
-      const node = table[key]
-      const parent = node.parent
-      if (parent === null) {
-        return ''
-      } else {
-        const name = node.name
-        return remakePath(parent,table) + '/' + name
-      }
-    }
+//     const remakePath = (key,table) => {
+//       const node = table[key]
+//       const parent = node.parent
+//       if (parent === null) {
+//         return ''
+//       } else {
+//         const name = node.name
+//         return remakePath(parent,table) + '/' + name
+//       }
+//     }
 
 
-    const ans = {}
-    for (let key in table) {
-      const path = remakePath(key,table)
-      mapOldToNewId[key] = path
-      const node = table[key]
+//     const ans = {}
+//     for (let key in table) {
+//       const path = remakePath(key,table)
+//       mapOldToNewId[key] = path
+//       const node = table[key]
 
-      const name = node.name
-      const content = node.content
-      const alias = content.alias
-      const comments = content.comments
+//       const name = node.name
+//       const content = node.content
+//       const alias = content.alias
+//       const comments = content.comments
 
-      let file_size = 0
-      let file_last_modified = 0
-      if (node.children.length === 0) {
-        file_size = content.size
-        file_last_modified = content.last_modified.max
-      }
+//       let file_size = 0
+//       let file_last_modified = 0
+//       if (node.children.length === 0) {
+//         file_size = content.size
+//         file_last_modified = content.last_modified.max
+//       }
 
-      ans[path] = {
-        name,
-        alias,
-        comments,
-        file_size,
-        file_last_modified,
+//       ans[path] = {
+//         name,
+//         alias,
+//         comments,
+//         file_size,
+//         file_last_modified,
 
-        children:[],
+//         children:[],
 
-        size:0,
-        last_modified_max:0,
-        last_modified_list:[],
-        last_modified_min:Number.MAX_SAFE_INTEGER,
-        last_modified_median:null,
-        last_modified_average:null,
-        depth:0,
-        nb_files:0,
-        sort_by_size_index:[],
-        sort_by_date_index:[],
-      }
-    }
+//         size:0,
+//         last_modified_max:0,
+//         last_modified_list:[],
+//         last_modified_min:Number.MAX_SAFE_INTEGER,
+//         last_modified_median:null,
+//         last_modified_average:null,
+//         depth:0,
+//         nb_files:0,
+//         sort_by_size_index:[],
+//         sort_by_date_index:[],
+//       }
+//     }
 
-    const computeChildren = (key) => {
-      const node = table[key]
-      const children = node.children
-      if (children.length) {
-        ans[mapOldToNewId[key]].children = children.map(a=>mapOldToNewId[a])
-        children.map(computeChildren)
-      }
-    }
+//     const computeChildren = (key) => {
+//       const node = table[key]
+//       const children = node.children
+//       if (children.length) {
+//         ans[mapOldToNewId[key]].children = children.map(a=>mapOldToNewId[a])
+//         children.map(computeChildren)
+//       }
+//     }
 
-    computeChildren(tree.root_id)
+//     computeChildren(tree.root_id)
 
-    return ans
-  }
+//     return ans
+//   }
   
-  v9.files_and_folders = v8TreeToV9Ffs(v8.tree)
+//   v9.files_and_folders = v8TreeToV9Ffs(v8.tree)
 
-  const v8TagsToV9Tags = tags => {
-    const ans = {}
-    for (let key in tags) {
-      ans[generateRandomString(40)] = {
-        name:key,
-        ff_ids:tags[key].map(a=>mapOldToNewId[a]),
-      }
-    }
-    return ans
-  }
+//   const v8TagsToV9Tags = tags => {
+//     const ans = {}
+//     for (let key in tags) {
+//       ans[generateRandomString(40)] = {
+//         name:key,
+//         ff_ids:tags[key].map(a=>mapOldToNewId[a]),
+//       }
+//     }
+//     return ans
+//   }
 
-  v9.tags = v8TagsToV9Tags(v8.tags)
+//   v9.tags = v8TagsToV9Tags(v8.tags)
 
-  return v9
-}
-
+//   return v9
+// }
+// DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
 
 
 export const v9JsToV10Js = (v9) => {

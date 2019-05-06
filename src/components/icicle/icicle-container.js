6e2ee0ad09a08735ee2bad70f019345c27691f64
Clément Berthou <clement.berthou.ext@francetv.fr>
2019-05-06 16:35:18 +0200

    Refactored Icicles to multiple files

---
+++ src/components/icicle/icicle-container.js
@@ -0,0 +1,37 @@
+import React from "react";
+import * as ObjectUtil from "util/object-util";
+import IcicleMain from './icicle-main';
+
+export default function IcicleApiToProps(props) {
+  const api = props.api;
+  const icicle_state = api.icicle_state;
+  const database = api.database;
+
+  const lock_sequence = icicle_state.lock_sequence();
+  const isLocked = lock_sequence.length > 0;
+
+  props = ObjectUtil.compose(
+    {
+      getFfByFfId: database.getFfByFfId,
+      root_id: database.rootFfId(),
+      display_root: icicle_state.display_root(),
+      getFfIdPath: database.getFfIdPath,
+      max_depth: database.maxDepth(),
+      isLocked,
+      sequence: icicle_state.sequence(),
+      hover_sequence: icicle_state.hover_sequence(),
+
+      setFocus: icicle_state.setFocus,
+      setNoFocus: icicle_state.setNoFocus,
+      lock: (...args) => {
+        icicle_state.lock(...args);
+        api.undo.commit();
+      },
+      unlock: icicle_state.unlock,
+      setDisplayRoot: icicle_state.setDisplayRoot
+    },
+    props
+  );
+
+  return <IcicleMain {...props} />;
+}
---
+++ src/components/icicle/svg-rectangle.js
@@ -0,0 +1,32 @@
+import React, { PureComponent } from 'react';
+
+export default class SvgRectangle extends PureComponent {
+    render() {
+        const props = this.props;
+        const x = props.x;
+        const y = props.y;
+        const dx = props.dx;
+        const dy = props.dy;
+        const onClickHandler = props.onClickHandler;
+        const onDoubleClickHandler = props.onDoubleClickHandler;
+        const onMouseOverHandler = props.onMouseOverHandler;
+        const fill = props.fill;
+        const opacity = props.opacity;
+        const stroke = props.stroke;
+        const cursor = props.cursor;
+
+        return (
+            <rect
+                className="node"
+                x={x}
+                y={y}
+                width={dx}
+                height={dy}
+                onClick={onClickHandler}
+                onDoubleClick={onDoubleClickHandler}
+                onMouseOver={onMouseOverHandler}
+                style={{ fill, opacity, stroke, cursor }}
+            />
+        );
+    }
+}

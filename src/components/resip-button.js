cdbc16722d78a8ab3ba77a3c41debc4bd4008133
Clément Berthou <clement.berthou@benextcompany.com>
2019-07-24 11:27:46 +0200

    Added Resip export button

---
+++ src/components/resip-button.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { mkB } from "components/button";
+
+import resipExporter from "../exporters/resipExporter";
+import { toStr } from "../csv";
+import { makeNameWithExt, save } from "../util/file-sys-util";
+
+const label = "RESIP";
+
+const ResipButton = props => {
+  const api = props.api;
+  const database = api.database;
+
+  const getSessionName = database.getSessionName;
+  const name = () => makeNameWithExt(`${getSessionName()}-RESIP`, "csv");
+  return mkB(
+    () => {
+      save(name(), toStr(resipExporter(database.getData())));
+    },
+    label,
+    true,
+    "#4d9e25",
+    { width: "90%" }
+  );
+};
+
+export default ResipButton;

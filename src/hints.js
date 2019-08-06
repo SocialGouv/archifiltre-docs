e86a716f6646e74d4008a1e2c3e3c6d808b0a76f
Jean-Baptiste Assouad <jean.baptiste@assouad.com>
2019-08-06 13:41:16 +0200

    add hints to loading screen

---
+++ src/hints.js
@@ -0,0 +1,16 @@
+import pick from "languages";
+
+export const hints = [
+  pick({
+    en: "teausitens ui staiun e",
+    fr: "ttttsssssssttrrrr"
+  }),
+  pick({
+    en: "boboboe",
+    fr: "oo..yyyy"
+  }),
+  pick({
+    en: "teaeaeauieaueie",
+    fr: "aesjjjjj"
+  })
+];

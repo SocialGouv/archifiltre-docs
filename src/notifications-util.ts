bf38f5900e51b2074c0246d742c1ff58b7ad6356
Clément Berthou <clement.berthou@benextcompany.com>
2019-10-28 17:19:52 +0100

    fix(file-drop): fix app being stuck when not dropping a file or a folder

--- src/notifications-util.ts
+++ src/notifications-util.ts
@@ -3,10 +3,19 @@ import { NotificationManager } from "react-notifications";
 const NOTIFICATION_TIMEOUT = 5000;
 
 /**
- * Display a success notification to the screen.
+ * Display a success notification on the screen.
  * @param message - The notification message
  * @param title - The notification title
  */
 export const notifySuccess = (message: string, title: string): void => {
   NotificationManager.success(message, title, NOTIFICATION_TIMEOUT);
+};
+
+/**
+ * Display an error notification on the screen.
+ * @param message - The notification message
+ * @param title - The notification title
+ */
+export const notifyError = (message: string, title: string): void => {
+  NotificationManager.error(message, title, NOTIFICATION_TIMEOUT);
 };

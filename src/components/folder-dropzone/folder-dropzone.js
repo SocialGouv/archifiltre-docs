5871c3035cff77f0aedb64776bf13d30ddc31e5c
Alezco <benjaminmorali4@hotmail.fr>
2019-12-04 17:41:59 +0100

    feat(loading): add specific message for JSON loading

--- src/components/folder-dropzone/folder-dropzone.js
+++ src/components/folder-dropzone/folder-dropzone.js
@@ -97,6 +97,7 @@ export default class FolderDropzone extends React.Component {
   }
 
   loadFileOrFolder(loadedPath) {
+    this.props.setLoadedPath(loadedPath);
     const {
       props: { api, setHashes }
     } = this;

c6b5ed4929a02266ef0da39430daf9d3ecbed047
tledoux <tledouxfr@gmail.com>
2019-12-10 22:30:21 +0100

    feat: add image, audio, video and archive category
    Divide the media category in image, audio and video.
    Add the archive category (for zip, tar, warc, ...).
    Put new colors : need to be made prettier...
    Adapt the audit report

--- src/util/file-types-util.test.ts
+++ src/util/file-types-util.test.ts
@@ -10,19 +10,9 @@ const createFileWithExt = (extension: string): FilesAndFolders =>
 
 describe("file-types-util", () => {
   describe("getFileType", () => {
-    it("should return the right type for media", () => {
-      const mkvFile = createFileWithExt("mkv");
-      expect(getFileType(mkvFile)).toBe(FileType.MEDIA);
-    });
-
-    it("should return the right type for spreadsheet", () => {
-      const xlsFile = createFileWithExt("xls");
-      expect(getFileType(xlsFile)).toBe(FileType.SPREADSHEET);
-    });
-
-    it("should return the right type for document", () => {
-      const docxFile = createFileWithExt("docx");
-      expect(getFileType(docxFile)).toBe(FileType.DOCUMENT);
+    it("should return the right type for publication", () => {
+      const pdfFile = createFileWithExt("pdf");
+      expect(getFileType(pdfFile)).toBe(FileType.PUBLICATION);
     });
 
     it("should return the right type for presentation", () => {
@@ -30,9 +20,39 @@ describe("file-types-util", () => {
       expect(getFileType(pptxFile)).toBe(FileType.PRESENTATION);
     });
 
+    it("should return the right type for spreadsheet", () => {
+      const xlsFile = createFileWithExt("xls");
+      expect(getFileType(xlsFile)).toBe(FileType.SPREADSHEET);
+    });
+
     it("should return the right type for email", () => {
       const emlFile = createFileWithExt("eml");
       expect(getFileType(emlFile)).toBe(FileType.EMAIL);
+    });
+
+    it("should return the right type for document", () => {
+      const docxFile = createFileWithExt("docx");
+      expect(getFileType(docxFile)).toBe(FileType.DOCUMENT);
+    });
+
+    it("should return the right type for image", () => {
+      const jpegFile = createFileWithExt("jpeg");
+      expect(getFileType(jpegFile)).toBe(FileType.IMAGE);
+    });
+
+    it("should return the right type for video", () => {
+      const mkvFile = createFileWithExt("mkv");
+      expect(getFileType(mkvFile)).toBe(FileType.VIDEO);
+    });
+
+    it("should return the right type for audio", () => {
+      const flacFile = createFileWithExt("flac");
+      expect(getFileType(flacFile)).toBe(FileType.AUDIO);
+    });
+
+    it("should return the right type for archive", () => {
+      const zipFile = createFileWithExt("zip");
+      expect(getFileType(zipFile)).toBe(FileType.OTHER);
     });
 
     it("should return the right type for unknown type", () => {

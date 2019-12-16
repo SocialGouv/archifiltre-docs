1bcf4b767360d81fe4c9ad383cadc46172130664
Alezco <benjaminmorali4@hotmail.fr>
2019-12-16 11:52:11 +0100

    feat(notifications): add possibility to display notifications for longer

--- src/util/notifications-util.ts
+++ src/util/notifications-util.ts
@@ -1,30 +1,49 @@
 import { NotificationManager } from "react-notifications";
 
-const NOTIFICATION_TIMEOUT = 5000;
+export enum NotificationTimeout {
+  SHORT = 3000,
+  NORMAL = 5000,
+  LONG = 10000
+}
 
 /**
  * Displays a success notification on the screen.
  * @param message - The notification message
  * @param title - The notification title
+ * @param notificationTimeout - The notification timeout (in ms)
  */
-export const notifySuccess = (message: string, title: string): void => {
-  NotificationManager.success(message, title, NOTIFICATION_TIMEOUT);
+export const notifySuccess = (
+  message: string,
+  title: string,
+  notificationTimeout = NotificationTimeout.NORMAL
+): void => {
+  NotificationManager.success(message, title, notificationTimeout);
 };
 
 /**
  * Displays an error notification on the screen.
  * @param message - The notification message
  * @param title - The notification title
+ * @param notificationTimeout - The notification timeout (in ms)
  */
-export const notifyError = (message: string, title: string): void => {
-  NotificationManager.error(message, title, NOTIFICATION_TIMEOUT);
+export const notifyError = (
+  message: string,
+  title: string,
+  notificationTimeout = NotificationTimeout.NORMAL
+): void => {
+  NotificationManager.error(message, title, notificationTimeout);
 };
 
 /**
  * Displays an info notification on the screen.
  * @param message - The notification message
  * @param title - The notification title
+ * @param notificationTimeout - The notification timeout (in ms)
  */
-export const notifyInfo = (message: string, title: string): void => {
-  NotificationManager.info(message, title, NOTIFICATION_TIMEOUT);
+export const notifyInfo = (
+  message: string,
+  title: string,
+  notificationTimeout = NotificationTimeout.NORMAL
+): void => {
+  NotificationManager.info(message, title, notificationTimeout);
 };

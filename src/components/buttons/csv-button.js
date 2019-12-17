38c8666f4bb2ed1e725ce5ade2a9a7558136146c
Clément Berthou <clement.berthou@benextcompany.com>
2019-12-17 18:19:30 +0100

    feat(hash): add csv with hashes export option

--- src/components/buttons/csv-button.js
+++ src/components/buttons/csv-button.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactTooltip from "react-tooltip";
 import { mkB } from "components/buttons/button";
 
 import { makeNameWithExt } from "util/file-sys-util";
@@ -10,17 +12,25 @@ const CsvButton = ({
   exportWithHashes = false
 }) => {
   const name = makeNameWithExt(database.getSessionName(), "csv");
-
   const { t } = useTranslation();
   const buttonLabel = exportWithHashes ? t("header.csvWithHash") : "CSV";
-  return mkB(
-    () => {
-      exportToCsv(name, { withHashes: exportWithHashes });
-    },
-    buttonLabel,
-    !exportWithHashes || areHashesReady,
-    "#4d9e25",
-    { width: "90%" }
+  const isActive = !exportWithHashes || areHashesReady;
+  return (
+    <span
+      data-tip={!isActive ? t("header.csvWithHashDisabledMessage") : ""}
+      data-for="disabledCSVTooltip"
+    >
+      {mkB(
+        () => {
+          exportToCsv(name, { withHashes: exportWithHashes });
+        },
+        buttonLabel,
+        isActive,
+        "#4d9e25",
+        { width: "90%" }
+      )}
+      {!isActive && <ReactTooltip id="disabledCSVTooltip" />}
+    </span>
   );
 };
 

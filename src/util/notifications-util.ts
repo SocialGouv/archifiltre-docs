58ae0b83ab0f350c800f8ed1d73f216e1fc00fc7
Alezco <benjaminmorali4@hotmail.fr>
2020-01-10 11:15:30 +0100

    feat(notifications): add permanent notifications

--- src/util/notifications-util.ts
+++ src/util/notifications-util.ts
@@ -1,49 +1,49 @@
 import { NotificationManager } from "react-notifications";
 
-export enum NotificationTimeout {
-  SHORT = 3000,
+export enum NotificationDuration {
   NORMAL = 5000,
-  LONG = 10000
+  LONG = 10000,
+  PERMANENT = Number.MAX_SAFE_INTEGER
 }
 
 /**
  * Displays a success notification on the screen.
  * @param message - The notification message
  * @param title - The notification title
- * @param notificationTimeout - The notification timeout (in ms)
+ * @param notificationDuration - The notification duration (in ms)
  */
 export const notifySuccess = (
   message: string,
   title: string,
-  notificationTimeout = NotificationTimeout.NORMAL
+  notificationDuration = NotificationDuration.NORMAL
 ): void => {
-  NotificationManager.success(message, title, notificationTimeout);
+  NotificationManager.success(message, title, notificationDuration);
 };
 
 /**
  * Displays an error notification on the screen.
  * @param message - The notification message
  * @param title - The notification title
- * @param notificationTimeout - The notification timeout (in ms)
+ * @param notificationDuration - The notification duration (in ms)
  */
 export const notifyError = (
   message: string,
   title: string,
-  notificationTimeout = NotificationTimeout.NORMAL
+  notificationDuration = NotificationDuration.NORMAL
 ): void => {
-  NotificationManager.error(message, title, notificationTimeout);
+  NotificationManager.error(message, title, notificationDuration);
 };
 
 /**
  * Displays an info notification on the screen.
  * @param message - The notification message
  * @param title - The notification title
- * @param notificationTimeout - The notification timeout (in ms)
+ * @param notificationDuration - The notification duration (in ms)
  */
 export const notifyInfo = (
   message: string,
   title: string,
-  notificationTimeout = NotificationTimeout.NORMAL
+  notificationDuration = NotificationDuration.NORMAL
 ): void => {
-  NotificationManager.info(message, title, notificationTimeout);
+  NotificationManager.info(message, title, notificationDuration);
 };

cd173e8e7a447a9c63e4839e1cde568a29181584
Clément Berthou <clement.berthou@benextcompany.com>
2020-01-16 11:19:39 +0100

    refactor(hash): refactor file hash worker to typescript

---
+++ src/hash-computer/file-hash-computer.impl.test.ts
@@ -0,0 +1,67 @@
+import { MessageTypes } from "../util/batch-process/batch-process-util-types";
+import { computeHash } from "../util/hash-util";
+import { onData, onInitialize } from "./file-hash-computer.impl";
+
+jest.mock("../util/hash-util", () => ({
+  computeHash: jest.fn()
+}));
+
+const basePath = "/base";
+const paths = ["/path1", "/path2"];
+
+const asyncWorker = {
+  addEventListener: jest.fn(),
+  postMessage: jest.fn(),
+  terminate: jest.fn()
+};
+
+const computeHashMock = computeHash as jest.Mock;
+
+describe("file-hash-computer.impl", () => {
+  beforeEach(() => {
+    computeHashMock.mockReset();
+  });
+  describe("onData", () => {
+    it("should handle valid paths", () => {
+      computeHashMock.mockImplementation((path: string) => `hash(${path})`);
+      onInitialize(asyncWorker, { basePath });
+
+      onData(asyncWorker, paths);
+
+      expect(asyncWorker.postMessage).toHaveBeenCalledWith({
+        result: [
+          { param: "/path1", result: "hash(/base/path1)" },
+          { param: "/path2", result: "hash(/base/path2)" }
+        ],
+        type: MessageTypes.RESULT
+      });
+    });
+
+    it("should handle errored paths", () => {
+      const error = new Error("test-error");
+      computeHashMock.mockImplementation((path: string) => {
+        if (path === "/base/path2") {
+          throw error;
+        }
+
+        return `hash(${path})`;
+      });
+      onInitialize(asyncWorker, { basePath });
+
+      onData(asyncWorker, paths);
+
+      expect(asyncWorker.postMessage).toHaveBeenCalledWith({
+        result: [
+          { param: "/path1", result: "hash(/base/path1)" },
+          { param: "/path2", result: null }
+        ],
+        type: MessageTypes.RESULT
+      });
+
+      expect(asyncWorker.postMessage).toHaveBeenCalledWith({
+        error: { param: "/path2", error: error.toString() },
+        type: MessageTypes.ERROR
+      });
+    });
+  });
+});

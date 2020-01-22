dc6e5ccca69b22a176f78bb959424720ce055bd8
Clément Berthou <clement.berthou@benextcompany.com>
2020-01-22 14:19:36 +0100

    feat(csv): display csv export loading info

--- src/exporters/csv/csv-exporter.ts
+++ src/exporters/csv/csv-exporter.ts
@@ -1,16 +1,24 @@
+import { promises as fs } from "fs";
 import { getFilesAndFoldersMetadataFromStore } from "reducers/files-and-folders-metadata/files-and-folders-metadata-selectors";
 import {
   getFilesAndFoldersFromStore,
   getHashesFromStore
 } from "reducers/files-and-folders/files-and-folders-selectors";
-import { last } from "rxjs/operators";
+import { from } from "rxjs";
+import { bufferTime, flatMap, last, tap } from "rxjs/operators";
 import { addTracker } from "../../logging/tracker";
 import { ActionTitle, ActionType } from "../../logging/tracker-types";
 import { ArchifiltreThunkAction } from "../../reducers/archifiltre-types";
+import {
+  completeLoadingAction,
+  progressLoadingAction
+} from "../../reducers/loading-info/loading-info-actions";
+import { startLoading } from "../../reducers/loading-info/loading-info-operations";
+import { LoadingInfoTypes } from "../../reducers/loading-info/loading-info-types";
 import { getTagsFromStore } from "../../reducers/tags/tags-selectors";
 import translations from "../../translations/translations";
-import { save, UTF8 } from "../../util/file-sys-util";
-import { notifyInfo } from "../../util/notifications-util";
+import { promptUserForSave } from "../../util/file-system-util";
+import { notifyInfo, notifySuccess } from "../../util/notifications-util";
 import {
   generateCsvExport$,
   GenerateCsvExportOptions
@@ -24,40 +32,72 @@ import {
 export const csvExporterThunk = (
   name: string,
   { withHashes = false } = {}
-): ArchifiltreThunkAction => (dispatch, getState) =>
-  new Promise(resolve => {
-    addTracker({
-      title: ActionTitle.CSV_EXPORT,
-      type: ActionType.TRACK_EVENT
-    });
+): ArchifiltreThunkAction => async (dispatch, getState) => {
+  const exportFilePath = await promptUserForSave(name);
 
-    const csvExportStartedMessage = translations.t(
-      "export.csvExportStartedMessage"
-    );
-    const csvExportTitle = translations.t("export.csvExportTitle");
-    notifyInfo(csvExportStartedMessage, csvExportTitle);
+  if (!exportFilePath) {
+    return;
+  }
 
-    const state = getState();
-    const tags = getTagsFromStore(state);
-    const filesAndFolders = getFilesAndFoldersFromStore(state);
-    const filesAndFoldersMetadata = getFilesAndFoldersMetadataFromStore(state);
-    const hashes = getHashesFromStore(state);
-
-    const data: GenerateCsvExportOptions = {
-      filesAndFolders,
-      filesAndFoldersMetadata,
-      hashes: undefined,
-      tags
-    };
-
-    if (withHashes) {
-      data.hashes = hashes;
-    }
-    const csvExportData$ = generateCsvExport$(data);
-    csvExportData$.pipe(last()).subscribe({
-      next: (csv: string) => {
-        save(name, csv, { format: UTF8 });
-        resolve();
-      }
-    });
+  addTracker({
+    title: ActionTitle.CSV_EXPORT,
+    type: ActionType.TRACK_EVENT
   });
+
+  const csvExportStartedMessage = translations.t(
+    "export.csvExportStartedMessage"
+  );
+  const csvExportTitle = translations.t("export.csvExportTitle");
+  notifyInfo(csvExportStartedMessage, csvExportTitle);
+
+  const state = getState();
+  const tags = getTagsFromStore(state);
+  const filesAndFolders = getFilesAndFoldersFromStore(state);
+  const filesAndFoldersMetadata = getFilesAndFoldersMetadataFromStore(state);
+  const hashes = getHashesFromStore(state);
+
+  const data: GenerateCsvExportOptions = {
+    filesAndFolders,
+    filesAndFoldersMetadata,
+    hashes: undefined,
+    tags
+  };
+
+  if (withHashes) {
+    data.hashes = hashes;
+  }
+
+  const nbFilesAndFolders = Object.keys(filesAndFolders).length;
+  const creatingCsvExportMessage = translations.t("export.creatingCsvExport");
+  const loadingId = dispatch(
+    startLoading(
+      LoadingInfoTypes.EXPORT,
+      nbFilesAndFolders,
+      creatingCsvExportMessage
+    )
+  );
+
+  const LOADING_BAR_UPDATE_INTERVAL = 1000;
+
+  const csvExportData$ = generateCsvExport$(data);
+  await new Promise(resolve => {
+    csvExportData$
+      .pipe(bufferTime(LOADING_BAR_UPDATE_INTERVAL))
+      .pipe(
+        tap(buffer => dispatch(progressLoadingAction(loadingId, buffer.length)))
+      )
+      .pipe(flatMap(buffer => from(buffer)))
+      .pipe(last())
+      .subscribe({
+        next: async (csv: string) => {
+          dispatch(completeLoadingAction(loadingId));
+          await fs.writeFile(exportFilePath, csv, { encoding: "utf-8" });
+          const csvExportSuccessMessage = translations.t(
+            "export.csvExportSuccessMessage"
+          );
+          notifySuccess(csvExportSuccessMessage, csvExportTitle);
+          resolve();
+        }
+      });
+  });
+};

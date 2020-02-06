e66e93885ac241eb697099c1724505f12efd9868
Clément Berthou <clement.berthou@benextcompany.com>
2020-02-06 10:36:50 +0100

    refactor: replace mkb function with button component

--- src/components/buttons/back-to-root-button.js
+++ src/components/buttons/back-to-root-button.js
@@ -1,51 +1,44 @@
-import React from "react";
-
-import * as ObjectUtil from "util/object-util";
-import { mkB } from "components/buttons/button";
-
-import * as Color from "util/color-util";
-
+import React, { useCallback } from "react";
 import { useTranslation } from "react-i18next";
+import Button, {
+  ButtonAngles,
+  ButtonColor,
+  ButtonSize
+} from "../common/button";
 
-const Presentational = props => {
+const Presentational = ({ backToRoot, isZoomed }) => {
   const { t } = useTranslation();
-  const buttonStyle = {
-    transition: "all 0.2s ease-out",
-    WebkitTransition: "all 0.2s ease-out",
-    padding: "0.3em 0.45em",
-    margin: "0",
-    borderRadius: "5px"
-  };
 
-  return mkB(
-    props.backToRoot,
-    <span>
-      <i className="fi-zoom-out" />
-      &ensp;{t("workspace.backToRoot")}
-    </span>,
-    props.isZoomed,
-    Color.parentFolder(),
-    buttonStyle
+  return (
+    <Button
+      id="zoom-out-button"
+      angles={ButtonAngles.ROUNDED}
+      color={ButtonColor.ICICLE_ACTION}
+      size={ButtonSize.SMALL}
+      onClick={backToRoot}
+      disabled={!isZoomed}
+    >
+      <span>
+        <i className="fi-zoom-out" />
+        &ensp;{t("workspace.backToRoot")}
+      </span>
+    </Button>
   );
 };
 
-export default props => {
-  const api = props.api;
-  const icicle_state = api.icicle_state;
+const BackToRoot = ({
+  api: {
+    icicle_state: { setNoDisplayRoot, setNoFocus, isZoomed },
+    undo: { commit }
+  }
+}) => {
+  const backToRoot = useCallback(() => {
+    setNoDisplayRoot();
+    setNoFocus();
+    commit();
+  }, [setNoDisplayRoot, setNoFocus, commit]);
 
-  const backToRoot = () => {
-    icicle_state.setNoDisplayRoot();
-    icicle_state.setNoFocus();
-    api.undo.commit();
-  };
-
-  props = ObjectUtil.compose(
-    {
-      isZoomed: icicle_state.isZoomed(),
-      backToRoot
-    },
-    props
-  );
-
-  return <Presentational {...props} />;
+  return <Presentational isZoomed={isZoomed()} backToRoot={backToRoot} />;
 };
+
+export default BackToRoot;

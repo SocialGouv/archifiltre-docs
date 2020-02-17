a9c6f9d4173c0e83022e7d8a726efb70383c7922
Clément Berthou <clement.berthou@benextcompany.com>
2020-02-17 15:21:09 +0100

    feat: allow to move elements with drag and drop

--- src/components/workspace/navigation-bar/navigation-bar-container.tsx
+++ src/components/workspace/navigation-bar/navigation-bar-container.tsx
@@ -1,6 +1,5 @@
 import React, { useCallback } from "react";
 import { useDispatch } from "react-redux";
-import { moveElement } from "../../../reducers/files-and-folders/files-and-folders-thunks";
 import { useWorkspaceMetadata } from "../../../reducers/workspace-metadata/workspace-metadata-selectors";
 import { setIciclesSortMethodThunk } from "../../../reducers/workspace-metadata/workspace-metadata-thunk";
 import { IciclesSortMethod } from "../../../reducers/workspace-metadata/workspace-metadata-types";
@@ -16,17 +15,11 @@ const NavigationBarContainer = ({ api }) => {
     [dispatch]
   );
 
-  const moveElementCallback = useCallback(
-    (elementId, parentId) => dispatch(moveElement(elementId, parentId)),
-    [dispatch]
-  );
-
   return (
     <NavigationBar
       api={api}
       iciclesSortMethod={iciclesSortMethod}
       setIciclesSortMethod={setIciclesSortMethodCallback}
-      moveElement={moveElementCallback}
     />
   );
 };

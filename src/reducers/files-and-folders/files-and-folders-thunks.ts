d858d2e44a1aa828cb5761f2d3c8a2f833e18349
Alezco <benjaminmorali4@hotmail.fr>
2020-02-21 16:33:21 +0100

    feat(dragndrop): block same name files/folders moves

--- src/reducers/files-and-folders/files-and-folders-thunks.ts
+++ src/reducers/files-and-folders/files-and-folders-thunks.ts
@@ -1,6 +1,9 @@
 import { createFilesAndFoldersMetadataDataStructure } from "../../files-and-folders-loader/files-and-folders-loader";
 import { addTracker } from "../../logging/tracker";
 import { ActionTitle, ActionType } from "../../logging/tracker-types";
+import translations from "../../translations/translations";
+import { isExactFileOrAncestor } from "../../util/file-and-folders-utils";
+import { notifyInfo } from "../../util/notifications-util";
 import { ArchifiltreThunkAction } from "../archifiltre-types";
 import { initFilesAndFoldersMetatada } from "../files-and-folders-metadata/files-and-folders-metadata-actions";
 import {
@@ -12,8 +15,10 @@ import {
 } from "./files-and-folders-actions";
 import {
   findElementParent,
-  getFilesAndFoldersFromStore
+  getFilesAndFoldersFromStore,
+  isFile
 } from "./files-and-folders-selectors";
+import { FilesAndFoldersMap } from "./files-and-folders-types";
 
 interface FfHashMap {
   [fileAndFoldersId: string]: string;
@@ -59,6 +64,44 @@ export const updateCommentThunk = (
   dispatch(addCommentsOnFilesAndFolders({ [filesAndFoldersId]: comments }));
 };
 
+export enum IsMoveValidError {
+  nameConflict = "nameConflict",
+  cannotMoveElement = "cannotMoveElement"
+}
+
+/**
+ * Returns an error if the move is invalid, null otherwise
+ * @param filesAndFolders - map of files and folders
+ * @param newParentId - id of target element
+ * @param elementId - id of moved element
+ */
+const isMoveValid = (
+  filesAndFolders: FilesAndFoldersMap,
+  newParentId: string,
+  elementId: string
+): IsMoveValidError | null => {
+  const newParentVirtualPath = filesAndFolders[newParentId].virtualPath;
+  const elementVirtualPath = filesAndFolders[elementId].virtualPath;
+  const newSiblingsNames = filesAndFolders[newParentId].children
+    .map(id => filesAndFolders[id])
+    .map(({ name }) => name);
+  const isNameConflict = newSiblingsNames.includes(
+    filesAndFolders[elementId].name
+  );
+  if (
+    isExactFileOrAncestor(newParentVirtualPath, elementVirtualPath) ||
+    isFile(filesAndFolders[newParentId])
+  ) {
+    return IsMoveValidError.cannotMoveElement;
+  }
+
+  if (isNameConflict) {
+    return IsMoveValidError.nameConflict;
+  }
+
+  return null;
+};
+
 /**
  * Allows to virtually move a file system element to another location
  * @param elementId
@@ -70,6 +113,15 @@ export const moveElement = (elementId, newParentId): ArchifiltreThunkAction => (
 ) => {
   const filesAndFolders = getFilesAndFoldersFromStore(getState());
   const parent = findElementParent(elementId, filesAndFolders);
+  const error = isMoveValid(filesAndFolders, newParentId, elementId);
+  if (error) {
+    const errorMessage =
+      error === IsMoveValidError.cannotMoveElement
+        ? translations.t("workspace.cannotMoveElement")
+        : translations.t("workspace.nameConflict");
+    notifyInfo(errorMessage, translations.t("workspace.impossibleMove"));
+    return;
+  }
 
   dispatch(removeChild(parent.id, elementId));
   dispatch(addChild(newParentId, elementId));

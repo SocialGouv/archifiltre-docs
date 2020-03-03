f3daf4917f519b19aa9a99dee290b034863f0a73
Alezco <benjaminmorali4@hotmail.fr>
2020-03-03 18:03:04 +0100

    feat(header): add link to wiki

---
+++ src/components/header/help-link.tsx
@@ -0,0 +1,38 @@
+import { shell } from "electron";
+import React, { FC } from "react";
+import { useTranslation } from "react-i18next";
+import { FaQuestionCircle } from "react-icons/all";
+import ReactTooltip from "react-tooltip";
+import styled from "styled-components";
+
+/**
+ * Opens the wiki in a browser
+ * @param event
+ */
+const onClick = event => {
+  event.preventDefault();
+  shell.openExternal(
+    "https://github.com/SocialGouv/archifiltre/wiki/Wiki-Archifiltre"
+  );
+};
+
+const QuestionCircle = styled(FaQuestionCircle)`
+  width: 0.6em;
+  vertical-align: middle;
+  color: dimgrey;
+  cursor: pointer;
+`;
+
+export const HelpLink: FC = () => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <QuestionCircle
+        onClick={onClick}
+        data-tip={t("header.help")}
+        data-for="help-button"
+      />
+      <ReactTooltip place="bottom" id="help-button" />
+    </>
+  );
+};

6f1592714463654b9a093df68ac00347669bfb1a
Alezco <benjaminmorali4@hotmail.fr>
2020-03-17 10:52:33 +0100

    feat(search): add react table

---
+++ src/components/search-modal/search-modal-container.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { getFilesAndFoldersFromStore } from "../../reducers/files-and-folders/files-and-folders-selectors";
+import { SearchModal } from "./search-modal";
+
+export const SearchModalContainer = ({ isModalOpen, closeModal }) => {
+  const filesAndFolders = useSelector(getFilesAndFoldersFromStore);
+
+  return (
+    <SearchModal
+      filesAndFolders={filesAndFolders}
+      isModalOpen={isModalOpen}
+      closeModal={closeModal}
+    />
+  );
+};

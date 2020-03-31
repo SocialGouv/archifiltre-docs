a4a97ebcfedce57495f00579652a0b0575fa0eb1
Alezco <benjaminmorali4@hotmail.fr>
2020-03-31 15:22:33 +0200

    chore(eslint): remove unused rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
     "jsx-a11y/click-events-have-key-events": "off",
     "jsx-a11y/interactive-supports-focus": "off",
     "jsx-a11y/no-static-element-interactions": "off",
-    "jsx-a11y/anchor-is-valid": "off",
   },
   settings: {
     "import/resolver": "webpack",

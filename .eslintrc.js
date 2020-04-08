d3e8e6be3a05c8ad2eec9a529ecbcb2a991ecc98
Benjamin Morali <benjaminmorali4@hotmail.fr>
2020-04-08 11:48:51 +0200

    chore(eslint): update eslint accessibility rules (#589)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: ["@socialgouv/eslint-config-react"],
   rules: {
     "react/prop-types": "off",
-    "jsx-a11y/mouse-events-have-key-events": "off",
     "jsx-a11y/click-events-have-key-events": "off",
     "jsx-a11y/interactive-supports-focus": "off",
     "jsx-a11y/no-static-element-interactions": "off",

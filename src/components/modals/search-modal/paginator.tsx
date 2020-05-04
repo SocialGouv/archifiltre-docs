f985b0de8e7965d0ed0f7db559fb10b9f10d01fd
Benjamin Morali <benjaminmorali4@hotmail.fr>
2020-05-04 10:41:07 +0200

    refactor(table): replace react table with MUI table (#710)

--- src/components/modals/search-modal/paginator.tsx
+++ src/components/modals/search-modal/paginator.tsx
@@ -1,70 +1,26 @@
+import TablePagination from "@material-ui/core/TablePagination";
 import React from "react";
-import { useTranslation } from "react-i18next";
-import {
-  FaAngleDoubleLeft,
-  FaAngleDoubleRight,
-  FaAngleLeft,
-  FaAngleRight,
-} from "react-icons/all";
-import styled from "styled-components";
-
-const PaginatorWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const PageIndicator = styled.span`
-  padding-left: 10px;
-  padding-right: 10px;
-`;
-
-const PageButton = styled.button`
-  &:hover {
-    background-color: #ededed;
-  }
-  cursor: pointer;
-  padding-left: 5px;
-  padding-right: 5px;
-  border-radius: 50%;
-  width: 25px;
-  height: 25px;
-`;
 
 const Paginator = ({
-  gotoPage,
-  canPreviousPage,
-  previousPage,
-  nextPage,
-  canNextPage,
   pageCount,
-  pageIndex,
-  pageOptions,
+  rowsPerPage,
+  page,
+  handleChangePage,
+  handleChangeRowsPerPage,
 }) => {
-  const { t } = useTranslation();
   return (
-    <PaginatorWrapper>
-      <PageButton onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-        <FaAngleDoubleLeft />
-      </PageButton>
-      <PageButton onClick={() => previousPage()} disabled={!canPreviousPage}>
-        <FaAngleLeft />
-      </PageButton>
-      <PageIndicator>
-        {`${t("search.page")} `}
-        <strong>
-          {pageIndex + 1}/{pageOptions.length}
-        </strong>
-      </PageIndicator>
-      <PageButton onClick={() => nextPage()} disabled={!canNextPage}>
-        <FaAngleRight />
-      </PageButton>
-      <PageButton
-        onClick={() => gotoPage(pageCount - 1)}
-        disabled={!canNextPage}
-      >
-        <FaAngleDoubleRight />
-      </PageButton>
-    </PaginatorWrapper>
+    <TablePagination
+      rowsPerPageOptions={[]}
+      component="div"
+      count={pageCount}
+      rowsPerPage={rowsPerPage}
+      page={page}
+      onChangePage={handleChangePage}
+      onChangeRowsPerPage={handleChangeRowsPerPage}
+      labelDisplayedRows={({ from, to, count }) =>
+        `${from}-${to === -1 ? count : to}/${count !== -1 ? count : ">" + to}`
+      }
+    />
   );
 };
 

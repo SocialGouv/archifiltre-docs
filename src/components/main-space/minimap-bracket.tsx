ac6e4c2a9b7e5b00d37af0d7b90aed10bc88e90b
Benjamin Morali <benjaminmorali4@hotmail.fr>
2020-05-19 16:46:32 +0200

    refactor(ts): refactor more ts components (#747)

---
+++ src/components/main-space/minimap-bracket.tsx
@@ -0,0 +1,51 @@
+import React, { FC, useMemo } from "react";
+
+type MinimapBracketProps = {
+  x: number;
+  y: number;
+  dx: number;
+  dy: number;
+  displayRoot: string;
+  computeWidthRec: (displayRoot: string, x: number, dx: number) => any;
+};
+
+const MinimapBracket: FC<MinimapBracketProps> = ({
+  x,
+  y,
+  dx,
+  dy,
+  displayRoot,
+  computeWidthRec,
+}) => {
+  if (displayRoot.length) {
+    return <g />;
+  }
+  const [minimapX, minimapUnboundWidth] = computeWidthRec(
+    displayRoot,
+    x,
+    dx
+  ).slice(-1)[0];
+
+  const minimapWidth = useMemo(() => Math.max(minimapUnboundWidth, 1), []);
+
+  return (
+    <g>
+      <rect
+        x={x}
+        y={y}
+        width={minimapX - x}
+        height={dy}
+        style={{ fill: "black", opacity: "0.4" }}
+      />
+      <rect
+        x={minimapX + minimapWidth}
+        y={y}
+        width={x + dx - (minimapX + minimapWidth)}
+        height={dy}
+        style={{ fill: "black", opacity: "0.4" }}
+      />
+    </g>
+  );
+};
+
+export default React.memo(MinimapBracket);

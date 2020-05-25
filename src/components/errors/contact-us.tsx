34c1465b590610629e61511eb1e90b5fef228072
Benjamin Morali <benjaminmorali4@hotmail.fr>
2020-05-25 14:55:49 +0200

    fix(error-screen): fix error screen layout (#755)

--- src/components/errors/contact-us.tsx
+++ src/components/errors/contact-us.tsx
@@ -1,12 +1,25 @@
+import useTheme from "@material-ui/core/styles/useTheme";
 import React, { FC } from "react";
 import { useTranslation } from "react-i18next";
 import { shell } from "electron";
+import styled from "styled-components";
+import { ThemedProps } from "../../theme/default-theme";
 
 const sendMailToArchifiltre = () => {
   shell.openExternal("mailto:archifiltre@sg.social.gouv.fr");
 };
 
+const ContactLink = styled.a<ThemedProps>`
+  cursor: pointer;
+  color: ${({ theme }) => theme.palette.primary.main};
+`;
+
 export const ContactUs: FC = () => {
   const { t } = useTranslation();
-  return <a onClick={() => sendMailToArchifiltre()}>{t("common.contactUs")}</a>;
+  const theme = useTheme();
+  return (
+    <ContactLink onClick={() => sendMailToArchifiltre()} theme={theme}>
+      {t("common.contactUs")}
+    </ContactLink>
+  );
 };

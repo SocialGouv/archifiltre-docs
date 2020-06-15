6cb3efa165773fedd42189329a953679dd0272f7
Alezco <benjaminmorali4@hotmail.fr>
2020-06-15 16:20:49 +0200

    refactor(all-tags): refactor all tags modal

--- src/components/tags/all-tags-container.tsx
+++ src/components/tags/all-tags-container.tsx
@@ -1,13 +1,7 @@
 import React, { FC, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  deleteTag,
-  renameTag,
-  tagFile,
-  untagFile,
-} from "../../reducers/tags/tags-actions";
-import { getTagsFromStore } from "../../reducers/tags/tags-selectors";
-import { useWorkspaceMetadata } from "../../reducers/workspace-metadata/workspace-metadata-selectors";
+import { deleteTag, renameTag } from "reducers/tags/tags-actions";
+import { getTagsFromStore } from "reducers/tags/tags-selectors";
 import AllTags from "./all-tags";
 
 interface AllTagsContainerProps {
@@ -19,8 +13,6 @@ const AllTagsContainer: FC<AllTagsContainerProps> = ({ api }) => {
 
   const dispatch = useDispatch();
 
-  const { lockedElementId, hoveredElementId } = useWorkspaceMetadata();
-
   const renameTagCallback = useCallback(
     (tagId, newName) => dispatch(renameTag(tagId, newName)),
     [dispatch]
@@ -30,27 +22,12 @@ const AllTagsContainer: FC<AllTagsContainerProps> = ({ api }) => {
     dispatch,
   ]);
 
-  const deleteTaggedCallback = useCallback(
-    (tagId, ffId) => dispatch(untagFile(tagId, ffId)),
-    [dispatch]
-  );
-
-  const addTaggedCallback = useCallback(
-    (tagId, ffId) => dispatch(tagFile(tagId, ffId)),
-    [dispatch]
-  );
-
-  const focusedNodeId = lockedElementId || hoveredElementId;
-
   return (
     <AllTags
       api={api}
       tags={tags}
-      focusedNodeId={focusedNodeId}
       renameTag={renameTagCallback}
       deleteTag={deleteTagCallback}
-      deleteTagged={deleteTaggedCallback}
-      addTagged={addTaggedCallback}
     />
   );
 };
--- src/components/tags/all-tags-item.tsx
+++ src/components/tags/all-tags-item.tsx
@@ -1,144 +1,71 @@
-import Grid, { GridProps } from "@material-ui/core/Grid";
-import React, { FC } from "react";
-
-import Tag from "components/tags/tag";
-import MultiLinesInput from "components/tags/multi-lines-input";
-import { FaTrash, FaTimes, FaPlus, FaPen } from "react-icons/fa";
-import styled from "styled-components";
-import { empty } from "util/function/function-util";
-import Item = Electron.Item;
-
-const CellShrink = styled(Grid)<GridProps>`
-  padding: 0 0.1em;
-`;
-
-const Container = styled.div`
-  position: relative;
-  width: 100%;
-  background: none;
-  margin: 0;
-  padding: 4px 0.5em;
-`;
-
-interface ItemProps {
-  opacity: number;
-}
-
-const Item = styled(Grid)<ItemProps & GridProps>`
-  opacity: ${({ opacity }) => opacity};
-  position: relative;
-  z-index: 1;
-`;
-
-interface BackgroundProps {
-  percentage: string | number;
-}
-
-const Background = styled.div<BackgroundProps>`
-  transition: all 0.4s;
-  -webkit-transition: all 0.4s;
-  height: 100%;
-  width: ${({ percentage }) => percentage}%;
-  opacity: 0.2;
-  background-color: rgb(10, 50, 100);
-`;
+import { Chip, IconButton } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import Tooltip from "@material-ui/core/Tooltip";
+import React, { FC, useCallback, useMemo, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { FaTrash, FaPen } from "react-icons/fa";
+import { percent } from "util/numbers/numbers-util";
+import EditableField from "../fields/editable-field";
 
 interface AllTagsItemProps {
   tag;
-  opacity: number;
-  percentage: number;
+  size: number;
+  totalVolume: number;
   deleteTag: any;
-  shoud_display_count: boolean;
-  tag_number: number;
-  node_has_tag: boolean;
-  removeTagFromNode: any;
-  addTagToNode: any;
-  editing: boolean;
+  tagNumber: number;
   renameTag: any;
-  stopEditingTag: any;
-  startEditingTag: any;
-  highlightTag: any;
 }
 
 const AllTagsItem: FC<AllTagsItemProps> = ({
   tag,
-  opacity,
-  percentage,
+  size,
+  totalVolume,
   deleteTag,
-  shoud_display_count,
-  tag_number,
-  node_has_tag,
-  removeTagFromNode,
-  addTagToNode,
-  editing,
+  tagNumber,
   renameTag,
-  stopEditingTag,
-  startEditingTag,
-  highlightTag,
 }) => {
-  const deleteBubble = (
-    <div className="tags_bubble tags_cross" onClick={deleteTag}>
-      <FaTrash style={{ width: "50%", height: "50%" }} />
-    </div>
-  );
+  const { t } = useTranslation();
+  const tooltipText = useMemo(() => t("workspace.tagItemTooltip"), [t]);
+  const [isEditing, setIsEditing] = useState(false);
 
-  const countOrActionBubble = shoud_display_count ? (
-    <div className="tags_bubble tags_count">{tag_number}</div>
-  ) : node_has_tag ? (
-    <div className="tags_bubble tags_cross" onClick={removeTagFromNode}>
-      <FaTimes style={{ width: "50%", height: "50%" }} />
-    </div>
-  ) : (
-    <div className="tags_bubble tags_add" onClick={addTagToNode}>
-      <FaPlus style={{ width: "50%", height: "50%" }} />
-    </div>
-  );
+  const startEditing = useCallback(() => setIsEditing(true), [setIsEditing]);
 
-  let tagPill;
-  if (editing) {
-    tagPill = (
-      <MultiLinesInput
-        value={tag}
-        onFinish={(value) => {
-          if (value !== "") {
-            renameTag(value);
-          }
-          stopEditingTag();
-        }}
-        autofocus={true}
-      />
-    );
-  } else {
-    tagPill = (
-      <Tag
-        text={tag}
-        editing={false}
-        clickHandler={startEditingTag}
-        removeHandler={empty}
-        custom_style=""
-      />
-    );
-  }
-
-  const pencil = editing ? (
-    <span />
-  ) : (
-    <FaPen
-      className="edit_hover_pencil"
-      style={{ opacity: "0.3", paddingLeft: "0.4em" }}
-    />
+  const onInputChange = useCallback(
+    (newValue) => {
+      renameTag(newValue);
+      setIsEditing(false);
+    },
+    [setIsEditing]
   );
 
   return (
-    <Container onMouseEnter={highlightTag}>
-      <Item container opacity={opacity}>
-        <CellShrink item>{deleteBubble}</CellShrink>
-        <CellShrink item>{countOrActionBubble}</CellShrink>
-        <CellShrink item>{tagPill}</CellShrink>
-        <CellShrink item>{pencil}</CellShrink>
-      </Item>
-      <Background className="background" percentage={percentage} />
-    </Container>
+    <Box display="flex">
+      <Box display="flex" flexDirection="column" justifyContent="center" pr={1}>
+        <IconButton size="small" onClick={deleteTag}>
+          <FaTrash />
+        </IconButton>
+      </Box>
+      <Box display="flex" flexDirection="column" justifyContent="center" pr={1}>
+        <Chip size="small" label={tag} />
+      </Box>
+      <Box display="flex" flexDirection="column" justifyContent="center" pr={1}>
+        <Tooltip title={tooltipText}>
+          <span>{`${tagNumber} (${percent(size, totalVolume)}%)`}</span>
+        </Tooltip>
+      </Box>
+      <Box display="flex" flexDirection="column" justifyContent="center">
+        {isEditing ? (
+          <EditableField
+            trimValue={true}
+            selectTextOnFocus={true}
+            value={tag}
+            onChange={onInputChange}
+          />
+        ) : (
+          <FaPen onClick={startEditing} />
+        )}
+      </Box>
+    </Box>
   );
 };
 

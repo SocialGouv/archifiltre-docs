f17193a77f9d344bee2a946e2f40b9aa3fd23342
Clément Berthou <cberthou@users.noreply.github.com>
2020-06-25 16:31:44 +0200

    feat: highlight moved element (#883)

---
+++ src/components/main-space/icicle/icicle-highlight-element.tsx
@@ -0,0 +1,56 @@
+import React, { FC } from "react";
+import SvgRectangle from "./svg-rectangle";
+import { DimsMap } from "./icicle";
+import { empty } from "../../../util/function/function-util";
+import { useAnimatedValue } from "../../../hooks/use-animation";
+import { useFileMoveActiveState } from "../../../hooks/use-file-move-active-state";
+
+type IcicleHightlightElementProps = {
+  dimsMap: DimsMap;
+  highlightedElementId: string;
+  highlightedElementTime: number;
+};
+
+const ICICLE_HIGHLIGHT_DURATION = 3000;
+
+const IcicleHightlightElement: FC<IcicleHightlightElementProps> = ({
+  dimsMap,
+  highlightedElementId,
+  highlightedElementTime,
+}) => {
+  const dims = dimsMap[highlightedElementId];
+  const { x, dx, y, dy } = dims;
+
+  const highlightedElementControl = `${highlightedElementId}:${highlightedElementTime}`;
+
+  const animatedOpacity = useAnimatedValue(
+    1,
+    0,
+    ICICLE_HIGHLIGHT_DURATION,
+    highlightedElementControl
+  );
+
+  const { isFileMoveActive } = useFileMoveActiveState();
+
+  return !dims || !highlightedElementId ? null : (
+    <g>
+      <SvgRectangle
+        x={x}
+        dx={dx}
+        y={y}
+        dy={dy}
+        fill="transparent"
+        stroke="red"
+        onClickHandler={empty}
+        onDoubleClickHandler={empty}
+        onMouseOverHandler={empty}
+        opacity={animatedOpacity}
+        cursor={isFileMoveActive ? "move" : "pointer"}
+        elementId={highlightedElementId}
+        strokeWidth={3}
+      />
+    </g>
+  );
+};
+
+export default IcicleHightlightElement;

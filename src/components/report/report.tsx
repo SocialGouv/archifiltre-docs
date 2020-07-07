896d60a18e256dd40b7124d5d049da3e008f927c
Benjamin Morali <benjaminmorali4@hotmail.fr>
2020-07-07 17:32:13 +0200

    refactor(tabs): refactor tab layout display (#938)

--- src/components/report/report.tsx
+++ src/components/report/report.tsx
@@ -1,13 +1,10 @@
-import { Divider } from "@material-ui/core";
-import TabContent from "components/workspace/tab-content";
+import TabContentHeader from "components/workspace/tabs/tab-content-header";
+import TabsLayout from "components/workspace/tabs/tabs-layout";
 import React, { FC } from "react";
 import { useTranslation } from "react-i18next";
-import Grid from "@material-ui/core/Grid";
 import { FilesAndFoldersMap } from "reducers/files-and-folders/files-and-folders-types";
 import SessionInfo from "./session-info/session-info";
 import { getFirstLevelName } from "util/files-and-folders/file-and-folders-utils";
-import Box from "@material-ui/core/Box";
-import CategoryTitle from "../common/category-title";
 import ElementCharacteristicsContainer from "../info-boxes/element-characteristics/element-characteristics-container";
 
 type ReportProps = {
@@ -35,51 +32,32 @@ const Report: FC<ReportProps> = ({
 
   const firstLevelName = getFirstLevelName(filesAndFolders);
 
+  const components = [
+    {
+      title: t("report.fileTree"),
+      content: (
+        <SessionInfo
+          sessionName={sessionName}
+          onChangeSessionName={setSessionName}
+          nbFolders={nbFolders}
+          nbFiles={nbFiles}
+          volume={volume}
+          oldestFileTimestamp={oldestFileTimestamp}
+          newestFileTimestamp={newestFileTimestamp}
+          firstLevelName={firstLevelName}
+        />
+      ),
+    },
+    {
+      title: t("report.element"),
+      content: <ElementCharacteristicsContainer />,
+    },
+  ];
+
   return (
-    <TabContent title={t("report.info")}>
-      <Box display="flex" height="100%">
-        <Box flex={1}>
-          <Box display="flex" flexDirection="column" height="100%">
-            <Box>
-              <CategoryTitle>{t("report.fileTree")}</CategoryTitle>
-            </Box>
-            <Box flexGrow={1}>
-              <Grid container>
-                <Grid item xs={12}>
-                  <SessionInfo
-                    sessionName={sessionName}
-                    onChangeSessionName={setSessionName}
-                    nbFolders={nbFolders}
-                    nbFiles={nbFiles}
-                    volume={volume}
-                    oldestFileTimestamp={oldestFileTimestamp}
-                    newestFileTimestamp={newestFileTimestamp}
-                    firstLevelName={firstLevelName}
-                  />
-                </Grid>
-              </Grid>
-            </Box>
-          </Box>
-        </Box>
-        <Box padding={2}>
-          <Divider orientation="vertical" />
-        </Box>
-        <Box flex={1}>
-          <Box display="flex" flexDirection="column" height="100%">
-            <Box>
-              <CategoryTitle>{t("report.element")}</CategoryTitle>
-            </Box>
-            <Box flexGrow={1}>
-              <Grid container>
-                <Grid item xs={12}>
-                  <ElementCharacteristicsContainer />
-                </Grid>
-              </Grid>
-            </Box>
-          </Box>
-        </Box>
-      </Box>
-    </TabContent>
+    <TabContentHeader title={t("report.info")}>
+      <TabsLayout components={components} />
+    </TabContentHeader>
   );
 };
 

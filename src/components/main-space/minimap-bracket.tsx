9242d8f8b30feae6e9c2d1ebd2f94b104d9d4a79
Clément Berthou <cberthou@users.noreply.github.com>
2020-07-13 12:46:03 +0200

    fix: fix icicles minimap brackets not being displayed (#966)

--- src/components/main-space/minimap-bracket.tsx
+++ src/components/main-space/minimap-bracket.tsx
@@ -1,10 +1,10 @@
-import React, { FC, useMemo } from "react";
+import React, { FC } from "react";
 
 type MinimapBracketProps = {
   x: number;
   y: number;
-  dx: number;
-  dy: number;
+  viewportWidth: number;
+  viewportHeight: number;
   displayRoot: string[];
   computeWidthRec: (displayRoot: string[], x: number, dx: number) => any;
 };
@@ -12,36 +12,36 @@ type MinimapBracketProps = {
 const MinimapBracket: FC<MinimapBracketProps> = ({
   x,
   y,
-  dx,
-  dy,
+  viewportWidth,
+  viewportHeight,
   displayRoot,
   computeWidthRec,
 }) => {
-  if (displayRoot.length) {
+  if (displayRoot.length === 0) {
     return <g />;
   }
   const [minimapX, minimapUnboundWidth] = computeWidthRec(
     displayRoot,
     x,
-    dx
+    viewportWidth
   ).slice(-1)[0];
 
-  const minimapWidth = useMemo(() => Math.max(minimapUnboundWidth, 1), []);
+  const minimapWidth = Math.max(minimapUnboundWidth, 1);
 
   return (
     <g>
       <rect
-        x={x}
-        y={y}
-        width={minimapX - x}
-        height={dy}
+        x={0}
+        y={0}
+        width={minimapX}
+        height={viewportHeight}
         style={{ fill: "black", opacity: "0.4" }}
       />
       <rect
         x={minimapX + minimapWidth}
         y={y}
-        width={x + dx - (minimapX + minimapWidth)}
-        height={dy}
+        width={viewportWidth - (minimapX + minimapWidth)}
+        height={viewportHeight}
         style={{ fill: "black", opacity: "0.4" }}
       />
     </g>

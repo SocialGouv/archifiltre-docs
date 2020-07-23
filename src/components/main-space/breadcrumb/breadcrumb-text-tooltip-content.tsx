80dca4195c47f888c6e4b18d5563448e4e7b8310
Clément Berthou <cberthou@users.noreply.github.com>
2020-07-23 12:00:31 +0200

    fix: fix breadcrumbs text being cut (#990)

---
+++ src/components/main-space/breadcrumb/breadcrumb-text-tooltip-content.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react";
+import { Box } from "@material-ui/core";
+import styled from "styled-components";
+import React from "react";
+
+const ItalicText = styled.span`
+  font-style: italic;
+`;
+
+type BreadcrumbTextTooltipContentProps = {
+  alias: string | null;
+  name: string;
+};
+
+const BreadcrumbTextTooltipContent: FC<BreadcrumbTextTooltipContentProps> = ({
+  alias,
+  name,
+}) =>
+  alias ? (
+    <Box>
+      <span>`${alias} `</span>
+      <ItalicText>({name})</ItalicText>
+    </Box>
+  ) : (
+    <Box>
+      <span>{name}</span>
+    </Box>
+  );
+
+export default BreadcrumbTextTooltipContent;
--- src/components/main-space/breadcrumb/breadcrumb-text.tsx
+++ src/components/main-space/breadcrumb/breadcrumb-text.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from "react";
 import { Box } from "@material-ui/core";
+import Tooltip from "@material-ui/core/Tooltip";
+import BreadcrumbTextTooltipContent from "components/main-space/breadcrumb/breadcrumb-text-tooltip-content";
 
 type BreadcrumbTextProps = {
   name: string;
@@ -7,26 +9,28 @@ type BreadcrumbTextProps = {
 };
 
 const BreadcrumbText: FC<BreadcrumbTextProps> = ({ name, alias }) => (
-  <Box height="100%" display="flex" maxHeight="0.875rem">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
-      width="100%"
-      height="100%"
-      viewBox="0 0 100 100"
-      preserveAspectRatio="xMinYMid meet"
-    >
-      <text x="0" y="100" fontSize="100" fill="black">
-        {alias ? (
-          <>
-            <tspan>{alias} </tspan>
-            <tspan fontStyle="italic">({name})</tspan>
-          </>
-        ) : (
-          <tspan>{name}</tspan>
-        )}
-      </text>
-    </svg>
+  <Box height="100%" display="flex" maxHeight="1.25rem">
+    <Tooltip title={<BreadcrumbTextTooltipContent alias={alias} name={name} />}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        width="100%"
+        height="100%"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMinYMid meet"
+      >
+        <text x="0" y="75" fontSize="75" fill="black">
+          {alias ? (
+            <>
+              <tspan>{alias} </tspan>
+              <tspan fontStyle="italic">({name})</tspan>
+            </>
+          ) : (
+            <tspan>{name}</tspan>
+          )}
+        </text>
+      </svg>
+    </Tooltip>
   </Box>
 );
 

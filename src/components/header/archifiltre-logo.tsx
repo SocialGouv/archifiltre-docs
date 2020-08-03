445fcaed659c8377deec983f95dede0161e9c2fa
Benjamin Morali <benjaminmorali4@hotmail.fr>
2020-08-03 11:35:33 +0200

    chore: move archifiltre info to settings (#997)

--- src/components/header/archifiltre-logo.tsx
+++ src/components/header/archifiltre-logo.tsx
@@ -1,50 +1,16 @@
-import Tooltip from "@material-ui/core/Tooltip";
-import withTheme from "@material-ui/core/styles/withTheme";
-import { shell } from "electron";
 import React, { FC } from "react";
-import { useTranslation } from "react-i18next";
 import styled from "styled-components";
-import { ThemedProps } from "theme/default-theme";
-import version, { versionName } from "../../version";
-import { HelpLink } from "./help-link";
-
-const ArchifiltreLogoWrapper = styled.span`
-  line-height: 1.5rem;
-`;
 
 const ArchifiltreLogoText = styled.div`
-  font-size: 1.6rem;
+  font-size: 2rem;
   letter-spacing: 0.16rem;
 `;
 
-const ArchifiltreVersionText = withTheme(styled.a<ThemedProps>`
-  cursor: pointer;
-  color: ${({ theme }) => theme.palette.primary.main};
-  font-size: 0.8em;
-`);
-
-const onClick = (event) => {
-  event.preventDefault();
-  shell.openExternal(`${ARCHIFILTRE_SITE_URL}/#changelog`);
-};
-
-const versionSubtitle = `v${version} ${versionName}`;
-
 const ArchifiltreLogo: FC = () => {
-  const { t } = useTranslation();
-  const title = t("report.whatsNew");
   return (
-    <ArchifiltreLogoWrapper>
-      <ArchifiltreLogoText>
-        <b>archifiltre</b>
-      </ArchifiltreLogoText>
-      <Tooltip title={title}>
-        <ArchifiltreVersionText onClick={onClick} target="_blank" role="link">
-          {versionSubtitle}
-        </ArchifiltreVersionText>
-      </Tooltip>
-      <HelpLink />
-    </ArchifiltreLogoWrapper>
+    <ArchifiltreLogoText>
+      <b>archifiltre</b>
+    </ArchifiltreLogoText>
   );
 };
 
--- src/components/modals/settings-modal/settings-modal.tsx
+++ src/components/modals/settings-modal/settings-modal.tsx
@@ -2,13 +2,14 @@ import { Box } from "@material-ui/core";
 import DialogContent from "@material-ui/core/DialogContent";
 import Dialog from "@material-ui/core/Dialog";
 import LanguagePicker from "components/header/dashboard/language";
+import About from "components/modals/settings-modal/about";
 import ModalMenu from "components/modals/settings-modal/modal-menu";
 import PrivacySettings from "components/modals/settings-modal/privacy-settings";
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import ModalHeader from "../../modals/modal-header";
 
-const availableSettings = [<LanguagePicker />, <PrivacySettings />];
+const availableSettings = [<LanguagePicker />, <PrivacySettings />, <About />];
 
 const SettingsModal = ({ isModalOpen, closeModal }) => {
   const { t } = useTranslation();

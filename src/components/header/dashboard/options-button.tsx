4b7fbbfd240b4d4dc946b7b57c06232a51cc9e2b
Benjamin Morali <benjaminmorali4@hotmail.fr>
2020-08-03 17:27:47 +0200

    chore: update settings button on first screen (#995)

---
+++ src/components/header/dashboard/options-button.tsx
@@ -0,0 +1,30 @@
+import SettingsButton from "components/header/dashboard/settings-button";
+import UserButton from "components/header/dashboard/user-button";
+import SettingsModal from "components/modals/settings-modal/settings-modal";
+import { useModal } from "hooks/use-modal";
+import React, { FC } from "react";
+
+type OptionsButtonProps = {
+  resetWorkspace: any;
+  shouldDisplayReset: boolean;
+};
+
+const OptionsButton: FC<OptionsButtonProps> = ({
+  resetWorkspace,
+  shouldDisplayReset,
+}) => {
+  const { isModalOpen, openModal, closeModal } = useModal();
+
+  return (
+    <div>
+      {shouldDisplayReset ? (
+        <UserButton resetWorkspace={resetWorkspace} openModal={openModal} />
+      ) : (
+        <SettingsButton openModal={openModal} />
+      )}
+      <SettingsModal isModalOpen={isModalOpen} closeModal={closeModal} />
+    </div>
+  );
+};
+
+export default OptionsButton;

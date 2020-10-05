7f29d7c6abf277e037539e4ae053aaccbe6f5d5c
Clément Berthou <cberthou@users.noreply.github.com>
2020-10-05 15:54:29 +0200

    chore: refactor csv export computation (#1046)

--- src/exporters/csv/csv-exporter.impl.ts
+++ src/exporters/csv/csv-exporter.impl.ts
@@ -1,141 +1,19 @@
-import _, { sortBy } from "lodash";
-import path from "path";
 import { FilesAndFoldersMetadataMap } from "reducers/files-and-folders-metadata/files-and-folders-metadata-types";
-import { getFilesAndFoldersDepth } from "reducers/files-and-folders/files-and-folders-selectors";
 import {
   AliasMap,
   CommentsMap,
   FilesAndFoldersMap,
 } from "reducers/files-and-folders/files-and-folders-types";
-import { Tag, TagMap } from "reducers/tags/tags-types";
+import { TagMap } from "reducers/tags/tags-types";
 import translations from "translations/translations";
 import { WorkerMessageHandler } from "util/async-worker/async-worker-util";
 import { MessageTypes } from "util/batch-process/batch-process-util-types";
 import { arrayToCsv } from "util/csv/csv-util";
-import {
-  getAllChildren,
-  getType,
-  isExactFileOrAncestor,
-} from "util/files-and-folders/file-and-folders-utils";
-import { formatPathForUserSystem } from "util/file-system/file-sys-util";
-import { hasDuplicate } from "util/duplicates/duplicates-util";
 import { HashesMap } from "reducers/hashes/hashes-types";
+import { exportToCsv } from "util/array-export/array-export";
+import { tap, toArray } from "rxjs/operators";
 
-interface MakeCsvHeaderOptions {
-  withHashes: boolean;
-  withFilesToDelete: boolean;
-}
-
-/**
- * Generates the csv header for the CSV export
- * @param tags
- * @param withHash
- */
-const makeCsvHeader = (
-  tags: Tag[],
-  { withHashes, withFilesToDelete }: MakeCsvHeaderOptions
-) => {
-  const header = [
-    "",
-    translations.t("csvHeader.path"),
-    translations.t("csvHeader.pathLength"),
-    translations.t("csvHeader.name"),
-    translations.t("csvHeader.extension"),
-    translations.t("csvHeader.size"),
-    translations.t("csvHeader.firstModified"),
-    translations.t("csvHeader.lastModified"),
-    translations.t("csvHeader.newPath"),
-    translations.t("csvHeader.newName"),
-    translations.t("csvHeader.description"),
-    translations.t("csvHeader.fileOrFolder"),
-    translations.t("csvHeader.depth"),
-    translations.t("csvHeader.fileCount"),
-    translations.t("csvHeader.type"),
-  ];
-
-  if (withHashes) {
-    header.push(
-      translations.t("csvHeader.hash"),
-      translations.t("csvHeader.duplicate")
-    );
-  }
-
-  if (withFilesToDelete) {
-    header.push(translations.t("common.toDelete"));
-  }
-
-  const tagNames = tags.map(({ name }, index) => `tag${index} : ${name}`);
-
-  header.push(...tagNames);
-
-  return header;
-};
-
-/**
- * Simple date formatting function for performance matters.
- * @param timestamp
- */
-const formatOutputDate = (timestamp) => {
-  const date = new Date(timestamp);
-  const day = `0${date.getUTCDate()}`.slice(-2);
-  const month = `0${date.getMonth() + 1}`.slice(-2);
-  const year = date.getUTCFullYear();
-  return `${day}/${month}/${year}`;
-};
-
-/**
- * Generates a map of the list of tag ids for each ffId
- * @param tags - The tags map
- */
-const tagIdByFfId = (tags: TagMap) => {
-  const tagIdByFfIdMap = {};
-  Object.keys(tags).forEach((tagId) => {
-    const tag = tags[tagId];
-    tag.ffIds.forEach((ffId) => {
-      if (!tagIdByFfIdMap[ffId]) {
-        tagIdByFfIdMap[ffId] = [];
-      }
-      tagIdByFfIdMap[ffId].push(tagId);
-    });
-  });
-
-  return tagIdByFfIdMap;
-};
-
-interface TagByFfIdMap {
-  [id: string]: string[];
-}
-
-/**
- * Get the list of all tags on the file and its ancestors
- * @param tagIdByFfIdMap
- * @param elementId
- */
-const getTagsForFileOrAncestors = (
-  tagIdByFfIdMap: TagByFfIdMap,
-  elementId
-): string[] =>
-  _(Object.keys(tagIdByFfIdMap))
-    .filter((ffId) => isExactFileOrAncestor(elementId, ffId))
-    .map((matchedFfId) => tagIdByFfIdMap[matchedFfId])
-    .flatten()
-    .value();
-
-/**
- * Get all children of elements to delete
- * @param filesAndFolders
- * @param elementsToDelete
- */
-const getChildrenToDelete = (filesAndFolders, elementsToDelete) => {
-  return _(elementsToDelete)
-    .flatMap((elementToDelete) =>
-      getAllChildren(filesAndFolders, elementToDelete)
-    )
-    .uniq()
-    .value();
-};
-
-interface CsvExporterData {
+type CsvExporterData = {
   aliases: AliasMap;
   comments: CommentsMap;
   filesAndFolders: FilesAndFoldersMap;
@@ -143,7 +21,7 @@ interface CsvExporterData {
   elementsToDelete: string[];
   hashes?: HashesMap;
   tags: TagMap;
-}
+};
 
 /**
  * Handles the initialize message for the CSV exporter fork
@@ -169,98 +47,29 @@ export const onInitialize: WorkerMessageHandler = async (
     tags,
   }: CsvExporterData
 ) => {
-  const elementsToDeleteWithChildren = getChildrenToDelete(
+  const array = await exportToCsv({
+    aliases,
+    comments,
+    elementsToDelete,
     filesAndFolders,
-    elementsToDelete
-  );
-  const orderedTags = sortBy(tags, "name");
-  const withHashes = hashes !== undefined;
-  const withFilesToDelete = elementsToDeleteWithChildren.length > 0;
-  const header = makeCsvHeader(orderedTags, { withHashes, withFilesToDelete });
-  const folderText = translations.t("common.folder");
-  const fileText = translations.t("common.file");
-  const toDeleteText = translations.t("common.toDelete");
-  const tagIdByFfIdMap = tagIdByFfId(tags);
-
-  const lines = Object.keys(filesAndFolders)
-    .filter((ffId) => ffId !== "")
-    .map((ffId): string[] => {
-      const currentFf = filesAndFolders[ffId];
-      const currentMetadata = filesAndFoldersMetadata[ffId];
-      const filePath = formatPathForUserSystem(ffId);
-      const pathLength = `${filePath.length}`;
-      const name = currentFf.name;
-      const extension = path.extname(name);
-      const size = `${currentMetadata.childrenTotalSize}`;
-      const firstModifiedDate = formatOutputDate(
-        currentMetadata.minLastModified
-      );
-      const lastModifiedDate = formatOutputDate(
-        currentMetadata.maxLastModified
-      );
-      const fileOrFolder =
-        currentFf.children.length === 0 ? fileText : folderText;
-      const newPath =
-        currentFf.virtualPath !== ffId
-          ? formatPathForUserSystem(currentFf.virtualPath)
-          : "";
-      const depth = `${getFilesAndFoldersDepth(ffId)}`;
-      const fileCount = `${currentMetadata.nbChildrenFiles}`;
-      const type = getType(currentFf);
-      const elementHasDuplicate = hasDuplicate(hashes, currentFf)
-        ? translations.t("common.yes")
-        : translations.t("common.no");
-
-      const line = [
-        "",
-        filePath,
-        pathLength,
-        name,
-        extension,
-        size,
-        firstModifiedDate,
-        lastModifiedDate,
-        newPath,
-        aliases[ffId] || "",
-        comments[ffId] || "",
-        fileOrFolder,
-        depth,
-        fileCount,
-        type,
-      ];
-
-      if (hashes) {
-        line.push(hashes[ffId], elementHasDuplicate);
-      }
-
-      if (withFilesToDelete) {
-        const fileToDeleteText = elementsToDeleteWithChildren.includes(ffId)
-          ? toDeleteText
-          : "";
-        line.push(fileToDeleteText);
-      }
-
-      const tagsForCurrentFile = getTagsForFileOrAncestors(
-        tagIdByFfIdMap,
-        ffId
-      );
-
-      const tagsArray = orderedTags.map((tag) =>
-        tagsForCurrentFile.includes(tag.id) ? tag.name : ""
-      );
-
-      line.push(...tagsArray);
-
-      asyncWorker.postMessage({
-        result: ffId,
-        type: MessageTypes.RESULT,
-      });
-
-      return line;
-    });
+    filesAndFoldersMetadata,
+    hashes,
+    tags,
+    translator: translations.t.bind(translations),
+  })
+    .pipe(
+      tap((row) =>
+        asyncWorker.postMessage({
+          result: row[0],
+          type: MessageTypes.RESULT,
+        })
+      )
+    )
+    .pipe(toArray())
+    .toPromise();
 
   asyncWorker.postMessage({
-    result: arrayToCsv([header, ...lines]),
+    result: arrayToCsv(array),
     type: MessageTypes.RESULT,
   });
 
---
+++ src/util/array-export/array-export.test.ts
@@ -0,0 +1,433 @@
+import { createFilesAndFoldersMetadata } from "reducers/files-and-folders-metadata/files-and-folders-metadata-test-utils";
+import { createFilesAndFolders } from "reducers/files-and-folders/files-and-folders-test-utils";
+import { formatPathForUserSystem } from "util/file-system/file-sys-util";
+import { exportToCsv } from "util/array-export/array-export";
+import { toArray } from "rxjs/operators";
+
+const tagName = "test-tag-1";
+const rootFolderId = "/root";
+const taggedFfId = "/root/folder";
+const firstChildId = "/root/folder/ff-id.txt";
+const firstChildVirtualPath = "/root/ff-id.txt";
+const tagId = "test-tag-id";
+const tagId2 = "test-tag-id-2";
+const rootId = "";
+const tag2Name = "tag2";
+const tags = {
+  [tagId]: {
+    ffIds: [taggedFfId],
+    id: tagId,
+    name: tagName,
+  },
+  [tagId2]: {
+    ffIds: [taggedFfId],
+    id: tagId2,
+    name: tag2Name,
+  },
+};
+
+const filesAndFolders = {
+  [rootId]: createFilesAndFolders({
+    children: [rootFolderId],
+    file_last_modified: 1571325669,
+    file_size: 10,
+    id: rootId,
+    name: "",
+  }),
+  [rootFolderId]: createFilesAndFolders({
+    children: [taggedFfId],
+    file_last_modified: 1571325669,
+    file_size: 10,
+    id: rootFolderId,
+    name: "root",
+  }),
+  [taggedFfId]: createFilesAndFolders({
+    children: [firstChildId],
+    file_last_modified: 1571325669,
+    file_size: 10,
+    id: taggedFfId,
+    name: "folder",
+  }),
+  [firstChildId]: createFilesAndFolders({
+    children: [],
+    file_last_modified: 1571325669,
+    file_size: 10,
+    id: firstChildId,
+    name: "ff-id.txt",
+    virtualPath: firstChildVirtualPath,
+  }),
+};
+
+const filesAndFoldersMetadata = {
+  [rootFolderId]: createFilesAndFoldersMetadata({
+    averageLastModified: 3000,
+    childrenTotalSize: 10000,
+    maxLastModified: 10000,
+    medianLastModified: 4000,
+    minLastModified: 1000,
+  }),
+  [taggedFfId]: createFilesAndFoldersMetadata({
+    averageLastModified: 3000,
+    childrenTotalSize: 10000,
+    maxLastModified: 10000,
+    medianLastModified: 4000,
+    minLastModified: 1000,
+  }),
+  [firstChildId]: createFilesAndFoldersMetadata({
+    averageLastModified: 3000,
+    childrenTotalSize: 10000,
+    maxLastModified: 10000,
+    medianLastModified: 4000,
+    minLastModified: 1000,
+  }),
+};
+
+const rootHash = "root-tag";
+const rootFolderHash = "root-folder-hash";
+const taggedHash = "tagged-hash";
+const firstChildIdHash = "tagged-hash";
+const hashes = {
+  [rootId]: rootHash,
+  [rootFolderId]: rootFolderHash,
+  [taggedFfId]: taggedHash,
+  [firstChildId]: firstChildIdHash,
+};
+
+const rootAlias = "root-alias";
+const firstChildIdAlias = "aliased-element";
+const aliases = {
+  [rootId]: rootAlias,
+  [firstChildId]: firstChildIdAlias,
+};
+
+const rootComment = "root-comment";
+const firstChildIdComment = "commented-element";
+const comments = {
+  [rootId]: rootComment,
+  [firstChildId]: firstChildIdComment,
+};
+
+const translator = (key) => `translate(${key})`;
+
+describe("array-export", () => {
+  describe("exportToCsv", () => {
+    it("should return the right array without hashes", async () => {
+      const csvHeader = [
+        "",
+        "translate(csvHeader.path)",
+        "translate(csvHeader.pathLength)",
+        "translate(csvHeader.name)",
+        "translate(csvHeader.extension)",
+        "translate(csvHeader.size)",
+        "translate(csvHeader.firstModified)",
+        "translate(csvHeader.lastModified)",
+        "translate(csvHeader.newPath)",
+        "translate(csvHeader.newName)",
+        "translate(csvHeader.description)",
+        "translate(csvHeader.fileOrFolder)",
+        "translate(csvHeader.depth)",
+        "translate(csvHeader.fileCount)",
+        "translate(csvHeader.type)",
+        `tag0 : ${tag2Name}`,
+        `tag1 : ${tagName}`,
+      ];
+      const csvFirstLine = [
+        "",
+        `${formatPathForUserSystem(rootFolderId)}`,
+        "5",
+        "root",
+        "",
+        "10000",
+        "01/01/1970",
+        "01/01/1970",
+        "",
+        "",
+        "",
+        translator("common.folder"),
+        "0",
+        "1",
+        translator("common.folder"),
+        "",
+        "",
+      ];
+      const csvSecondLine = [
+        "",
+        `${formatPathForUserSystem(taggedFfId)}`,
+        "12",
+        "folder",
+        "",
+        "10000",
+        "01/01/1970",
+        "01/01/1970",
+        "",
+        "",
+        "",
+        translator("common.folder"),
+        "1",
+        "1",
+        translator("common.folder"),
+        `${tag2Name}`,
+        `${tagName}`,
+      ];
+      const csvThirdLine = [
+        "",
+        `${formatPathForUserSystem(firstChildId)}`,
+        "22",
+        "ff-id.txt",
+        ".txt",
+        "10000",
+        "01/01/1970",
+        "01/01/1970",
+        `${formatPathForUserSystem(firstChildVirtualPath)}`,
+        `${aliases[firstChildId]}`,
+        `${comments[firstChildId]}`,
+        translator("common.file"),
+        "2",
+        "1",
+        "plain",
+        `${tag2Name}`,
+        `${tagName}`,
+      ];
+      const expectedCsv = [
+        csvHeader,
+        csvFirstLine,
+        csvSecondLine,
+        csvThirdLine,
+      ];
+
+      expect(
+        await exportToCsv({
+          aliases,
+          comments,
+          filesAndFolders,
+          filesAndFoldersMetadata,
+          tags,
+          elementsToDelete: [],
+          translator,
+        })
+          .pipe(toArray())
+          .toPromise()
+      ).toEqual(expectedCsv);
+    });
+
+    it("should return the right csv with hashes", async () => {
+      const csvHeader = [
+        "",
+        "translate(csvHeader.path)",
+        "translate(csvHeader.pathLength)",
+        "translate(csvHeader.name)",
+        "translate(csvHeader.extension)",
+        "translate(csvHeader.size)",
+        "translate(csvHeader.firstModified)",
+        "translate(csvHeader.lastModified)",
+        "translate(csvHeader.newPath)",
+        "translate(csvHeader.newName)",
+        "translate(csvHeader.description)",
+        "translate(csvHeader.fileOrFolder)",
+        "translate(csvHeader.depth)",
+        "translate(csvHeader.fileCount)",
+        "translate(csvHeader.type)",
+        "translate(csvHeader.hash)",
+        "translate(csvHeader.duplicate)",
+        `tag0 : ${tag2Name}`,
+        `tag1 : ${tagName}`,
+      ];
+      const csvFirstLine = [
+        "",
+        `${formatPathForUserSystem(rootFolderId)}`,
+        "5",
+        "root",
+        "",
+        "10000",
+        "01/01/1970",
+        "01/01/1970",
+        "",
+        "",
+        "",
+        translator("common.folder"),
+        "0",
+        "1",
+        translator("common.folder"),
+        rootFolderHash,
+        translator("common.no"),
+        "",
+        "",
+      ];
+      const csvSecondLine = [
+        "",
+        `${formatPathForUserSystem(taggedFfId)}`,
+        "12",
+        "folder",
+        "",
+        "10000",
+        "01/01/1970",
+        "01/01/1970",
+        "",
+        "",
+        "",
+        translator("common.folder"),
+        "1",
+        "1",
+        translator("common.folder"),
+        taggedHash,
+        translator("common.yes"),
+        `${tag2Name}`,
+        `${tagName}`,
+      ];
+      const csvThirdLine = [
+        "",
+        `${formatPathForUserSystem(firstChildId)}`,
+        "22",
+        "ff-id.txt",
+        ".txt",
+        "10000",
+        "01/01/1970",
+        "01/01/1970",
+        `${formatPathForUserSystem(firstChildVirtualPath)}`,
+        `${aliases[firstChildId]}`,
+        `${comments[firstChildId]}`,
+        translator("common.file"),
+        "2",
+        "1",
+        "plain",
+        firstChildIdHash,
+        translator("common.yes"),
+        `${tag2Name}`,
+        `${tagName}`,
+      ];
+      const expectedCsv = [
+        csvHeader,
+        csvFirstLine,
+        csvSecondLine,
+        csvThirdLine,
+      ];
+
+      expect(
+        await exportToCsv({
+          aliases,
+          comments,
+          elementsToDelete: [],
+          filesAndFolders,
+          filesAndFoldersMetadata,
+          hashes,
+          tags,
+          translator,
+        })
+          .pipe(toArray())
+          .toPromise()
+      ).toEqual(expectedCsv);
+    });
+
+    it("should handle elementsToDelete", async () => {
+      const csvHeader = [
+        "",
+        "translate(csvHeader.path)",
+        "translate(csvHeader.pathLength)",
+        "translate(csvHeader.name)",
+        "translate(csvHeader.extension)",
+        "translate(csvHeader.size)",
+        "translate(csvHeader.firstModified)",
+        "translate(csvHeader.lastModified)",
+        "translate(csvHeader.newPath)",
+        "translate(csvHeader.newName)",
+        "translate(csvHeader.description)",
+        "translate(csvHeader.fileOrFolder)",
+        "translate(csvHeader.depth)",
+        "translate(csvHeader.fileCount)",
+        "translate(csvHeader.type)",
+        "translate(csvHeader.hash)",
+        "translate(csvHeader.duplicate)",
+        "translate(csvHeader.toDelete)",
+        `tag0 : ${tag2Name}`,
+        `tag1 : ${tagName}`,
+      ];
+      const csvFirstLine = [
+        "",
+        `${formatPathForUserSystem(rootFolderId)}`,
+        "5",
+        "root",
+        "",
+        "10000",
+        "01/01/1970",
+        "01/01/1970",
+        "",
+        "",
+        "",
+        translator("common.folder"),
+        "0",
+        "1",
+        translator("common.folder"),
+        rootFolderHash,
+        translator("common.no"),
+        "",
+        "",
+        "",
+      ];
+      const csvSecondLine = [
+        "",
+        `${formatPathForUserSystem(taggedFfId)}`,
+        "12",
+        "folder",
+        "",
+        "10000",
+        "01/01/1970",
+        "01/01/1970",
+        "",
+        "",
+        "",
+        translator("common.folder"),
+        "1",
+        "1",
+        translator("common.folder"),
+        taggedHash,
+        translator("common.yes"),
+        "",
+        `${tag2Name}`,
+        `${tagName}`,
+      ];
+      const csvThirdLine = [
+        "",
+        `${formatPathForUserSystem(firstChildId)}`,
+        "22",
+        "ff-id.txt",
+        ".txt",
+        "10000",
+        "01/01/1970",
+        "01/01/1970",
+        `${formatPathForUserSystem(firstChildVirtualPath)}`,
+        `${aliases[firstChildId]}`,
+        `${comments[firstChildId]}`,
+        translator("common.file"),
+        "2",
+        "1",
+        "plain",
+        firstChildIdHash,
+        translator("common.yes"),
+        translator("common.toDelete"),
+        `${tag2Name}`,
+        `${tagName}`,
+      ];
+      const expectedCsv = [
+        csvHeader,
+        csvFirstLine,
+        csvSecondLine,
+        csvThirdLine,
+      ];
+
+      expect(
+        await exportToCsv({
+          aliases,
+          comments,
+          elementsToDelete: ["/root/folder/ff-id.txt"],
+          filesAndFolders,
+          filesAndFoldersMetadata,
+          hashes,
+          tags,
+          translator,
+        })
+          .pipe(toArray())
+          .toPromise()
+      ).toEqual(expectedCsv);
+    });
+  });
+});

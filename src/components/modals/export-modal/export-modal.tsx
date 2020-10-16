376ad0858f94ccac8bafed458401c1cca1d24b62
Benjamin Morali <benjaminmorali4@hotmail.fr>
2020-10-16 15:19:08 +0200

    feat(export-modal): update export modal styles (#1068)

--- src/components/modals/export-modal/export-modal.tsx
+++ src/components/modals/export-modal/export-modal.tsx
@@ -1,6 +1,8 @@
 import Dialog from "@material-ui/core/Dialog";
 import React, { FC } from "react";
 import { useTranslation } from "react-i18next";
+import { theme } from "theme/theme";
+import { ThemeProvider } from "@material-ui/core/styles";
 import ModalHeader from "../../modals/modal-header";
 import ExportModalContent from "./export-modal-content-container";
 
@@ -13,10 +15,12 @@ const ExportModal: FC<ExportModalProps> = ({ isModalOpen, closeModal }) => {
   const { t } = useTranslation();
 
   return (
-    <Dialog open={isModalOpen} onClose={closeModal} fullWidth maxWidth="sm">
-      <ModalHeader title={t("exportModal.title")} onClose={closeModal} />
-      <ExportModalContent closeModal={closeModal} />
-    </Dialog>
+    <ThemeProvider theme={theme}>
+      <Dialog open={isModalOpen} onClose={closeModal} fullWidth maxWidth="sm">
+        <ModalHeader title={t("exportModal.title")} onClose={closeModal} />
+        <ExportModalContent closeModal={closeModal} />
+      </Dialog>
+    </ThemeProvider>
   );
 };
 

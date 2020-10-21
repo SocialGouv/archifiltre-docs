18e7a822724c247f266469a2a919560fd08e4c55
Clément Berthou <cberthou@users.noreply.github.com>
2020-10-21 18:18:14 +0200

    feat: export search table to csv (#1083)

--- src/util/table/table-util.test.ts
+++ src/util/table/table-util.test.ts
@@ -1,8 +1,11 @@
 import {
   accessorToFunction,
+  applyAccessorToTableValue,
   getComparator,
   stableSort,
+  tableContentToArray,
 } from "util/table/table-util";
+import { Column } from "components/common/table/table-types";
 
 describe("table-util", () => {
   describe("stableSort", () => {
@@ -38,6 +41,70 @@ describe("table-util", () => {
         { id: "id3", name: "name3" },
         { id: "id2", name: "name2" },
         { id: "id1", name: "name1" },
+      ]);
+    });
+  });
+
+  describe("applyAccessorToTableValue", () => {
+    type TestData = {
+      name: string;
+    };
+
+    it("should work with a function accessor", () => {
+      const data: TestData = {
+        name: "test-name",
+      };
+      const accessor = (value, index) => `${value.name}-${index}`;
+
+      expect(applyAccessorToTableValue(data, accessor, 1)).toBe("test-name-1");
+    });
+
+    it("should work with a prop accessor", () => {
+      const data: TestData = {
+        name: "test-name",
+      };
+
+      expect(applyAccessorToTableValue(data, "name")).toBe("test-name");
+    });
+  });
+
+  describe("tableContentToArray", () => {
+    type TestData = {
+      name: string;
+      value: string;
+    };
+
+    it("should generate a string[][] from table data and config", () => {
+      const data: TestData[] = [
+        {
+          name: "first-name",
+          value: "first-value",
+        },
+        {
+          name: "second-name",
+          value: "second-value",
+        },
+      ];
+
+      const columns: Column<TestData>[] = [
+        {
+          id: "name",
+          name: "name",
+          accessor: "name",
+        },
+        {
+          id: "value",
+          name: "value",
+          accessor: ({ value }) => `accessor(${value})`,
+        },
+      ];
+
+      const table = tableContentToArray(data, columns);
+
+      expect(table).toEqual([
+        ["name", "value"],
+        ["first-name", "accessor(first-value)"],
+        ["second-name", "accessor(second-value)"],
       ]);
     });
   });

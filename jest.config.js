5da9722a80c759fc34c77f18edf11b05ed763078
Clément Berthou <cberthou@users.noreply.github.com>
2020-10-22 15:07:32 +0200

    feat: persist user language choice (#1086)

--- jest.config.js
+++ jest.config.js
@@ -5,12 +5,12 @@ module.exports = {
   coverageDirectory: "coverage",
   globals: {
     FORCE_TRACKING: false,
-    MODE: "development",
+    MODE: "test",
     SENTRY_DSN: "https://sentry-mock-url.io",
     STATIC_ASSETS_PATH: ".",
   },
   moduleDirectories: ["src", "node_modules"],
   moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
   preset: "ts-jest/presets/js-with-ts",
-  setupFiles: ["jest-date-mock"],
+  setupFiles: ["jest-date-mock", "<rootDir>/test-util/mock-electron.js"],
 };

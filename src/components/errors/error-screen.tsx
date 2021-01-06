bfa7fea2041ce41b2b265f901815934dd255fe4d
Benjamin Morali <benjaminmorali4@hotmail.fr>
2021-01-06 13:11:01 +0100

    feat(error-screen): add reload button on error screen (#1140)

--- src/components/errors/error-screen.tsx
+++ src/components/errors/error-screen.tsx
@@ -1,10 +1,39 @@
 import React, { FC } from "react";
-import ArchifiltreMessage from "./archifiltre-message";
 import { useTranslation } from "react-i18next";
+import { ContactUs } from "./contact-us";
+import { Box } from "@material-ui/core";
+import logo from "../../../static/imgs/logo.png";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { reloadApp } from "util/electron/electron-util";
 
 const ErrorScreen: FC = () => {
   const { t } = useTranslation();
-  return <ArchifiltreMessage>{t("common.error")}</ArchifiltreMessage>;
+
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      height="100%"
+      flexDirection="column"
+    >
+      <Box p={2}>
+        <img alt="logo-archifiltre" src={logo} width={222} height={33} />
+      </Box>
+      <Box p={2}>
+        <Typography variant="h4">{t("common.error")}</Typography>
+      </Box>
+      <Box p={2}>
+        <ContactUs />
+      </Box>
+      <Box p={2}>
+        <Button variant="outlined" color="primary" onClick={reloadApp}>
+          Revenir à l'écran d'accueil
+        </Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default ErrorScreen;

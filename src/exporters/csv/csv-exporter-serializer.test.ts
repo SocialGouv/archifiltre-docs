b5f2c70a83c91556e7f0933bbbbb811d99c3b71c
Clément Berthou <clement.berthou@benextcompany.com>
2021-01-22 12:10:42 +0100

    chore: serialize data when sending them to csv exporter

---
+++ src/exporters/csv/csv-exporter-serializer.test.ts
@@ -0,0 +1,54 @@
+import { createFilesAndFolders } from "files-and-folders-loader/files-and-folders-loader";
+import { MockWritable } from "stdio-mock";
+import Stream from "stream";
+import { Language, WithLanguage } from "hooks/use-language";
+import { GenerateCsvExportOptions } from "exporters/csv/csv-exporter.controller";
+import { createTag } from "reducers/tags/tags-test-util";
+import {
+  parseCsvExporterOptionsFromStream,
+  stringifyCsvExporterOptionsToStream,
+} from "exporters/csv/csv-exporter-serializer";
+import { createFilesAndFoldersMetadata } from "reducers/files-and-folders-metadata/files-and-folders-metadata-test-utils";
+
+const extractDataFromMock = (writeable: MockWritable): Promise<Buffer[]> =>
+  new Promise((resolve) => {
+    writeable.on("finish", () => {
+      resolve(writeable.data());
+    });
+  });
+
+describe("csv-exporter-serializer", () => {
+  it("should send and parse a CsvExporterOptions", async () => {
+    const exporterOptions: WithLanguage<GenerateCsvExportOptions> = {
+      aliases: {},
+      comments: {},
+      elementsToDelete: [],
+      filesAndFolders: {
+        ff: createFilesAndFolders({ id: "ff" }),
+      },
+      filesAndFoldersMetadata: {
+        ff: createFilesAndFoldersMetadata({}),
+      },
+      hashes: {
+        ff: "ff-hash",
+      },
+      language: Language.FR,
+      tags: {
+        tag: createTag({ id: "tag" }),
+      },
+    };
+
+    const writeable = new MockWritable();
+
+    // @ts-ignore
+    stringifyCsvExporterOptionsToStream(writeable, exporterOptions);
+
+    const sentData: Buffer[] = await extractDataFromMock(writeable);
+
+    const parsedOptions = await parseCsvExporterOptionsFromStream(
+      Stream.Readable.from(sentData)
+    );
+
+    expect(parsedOptions).toEqual(exporterOptions);
+  });
+});
--- src/util/async-worker/async-worker-util.ts
+++ src/util/async-worker/async-worker-util.ts
@@ -60,6 +60,7 @@ export const makeChildWorkerMessageCallback = (
         await onInitialize(asyncWorker, data);
         asyncWorker.postMessage({ type: MessageTypes.READY });
       } catch (err) {
+        console.error(err);
         asyncWorker.postMessage({
           type: MessageTypes.FATAL,
           error: err.toString(),
@@ -74,6 +75,7 @@ export const makeChildWorkerMessageCallback = (
       try {
         await onData(asyncWorker, data);
       } catch (err) {
+        console.error(err);
         asyncWorker.postMessage({
           type: MessageTypes.FATAL,
           error: err.toString(),

e2c0e42f28d3bcdde15109eb08ef69f55e39456b
Clément Berthou <cberthou@users.noreply.github.com>
2021-01-26 16:07:00 +0100

    chore: serialize tree csv export data (#1168)

--- src/exporters/csv/tree-csv-exporter.fork.ts
+++ src/exporters/csv/tree-csv-exporter.fork.ts
@@ -1,8 +1,13 @@
 import { setupChildWorkerListeners } from "util/async-worker/async-worker-util";
 import { onInitialize } from "./tree-csv-exporter.impl";
 import { createAsyncWorkerForChildProcess } from "util/async-worker/child-process";
+import { MessageTypes } from "util/batch-process/batch-process-util-types";
+import { parseTreeCsvExporterOptionsFromStream } from "exporters/csv/tree-csv-exporter-serializer";
 
-const asyncWorker = createAsyncWorkerForChildProcess();
+const asyncWorker = createAsyncWorkerForChildProcess(async (stream) => ({
+  type: MessageTypes.INITIALIZE,
+  data: await parseTreeCsvExporterOptionsFromStream(stream),
+}));
 
 setupChildWorkerListeners(asyncWorker, {
   onInitialize,

b29260e5dc4f59f7b692610192adfe7548d258ab
Clément Berthou <cberthou@users.noreply.github.com>
2021-01-29 16:32:05 +0100

    chore: serialize excel export params (#1169)

--- src/exporters/excel/excel-exporter-controller.ts
+++ src/exporters/excel/excel-exporter-controller.ts
@@ -2,12 +2,26 @@ import translations from "translations/translations";
 import { backgroundWorkerProcess$ } from "util/batch-process/batch-process-util";
 import { CsvExporterData } from "exporters/csv/csv-exporter.impl";
 import { createAsyncWorkerForChildProcessControllerFactory } from "util/async-worker/child-process";
+import {
+  InitializeMessage,
+  MessageTypes,
+} from "util/batch-process/batch-process-util-types";
+import { stringifyCsvExporterOptionsToStream } from "exporters/csv/csv-exporter-serializer";
+
+const initMessageSerializer = (stream, { data }: InitializeMessage) =>
+  stringifyCsvExporterOptionsToStream(stream, data);
+
+const messageSerializers = {
+  [MessageTypes.INITIALIZE]: initMessageSerializer,
+};
 
 export const generateExcelExport$ = (data: CsvExporterData) => {
   const { language } = translations;
 
   return backgroundWorkerProcess$(
     { ...data, language },
-    createAsyncWorkerForChildProcessControllerFactory("excel-exporter.fork")
+    createAsyncWorkerForChildProcessControllerFactory("excel-exporter.fork", {
+      messageSerializers,
+    })
   );
 };

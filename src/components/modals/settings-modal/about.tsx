356907fcde98a4b6bd716a6d9e198848713a4b23
Alezco <benjaminmorali4@hotmail.fr>
2021-02-04 11:24:15 +0100

    feat(settings): add more links in settings modal

--- src/components/modals/settings-modal/about.tsx
+++ src/components/modals/settings-modal/about.tsx
@@ -1,47 +1,81 @@
-import { Link } from "@material-ui/core";
+import { createStyles, Link } from "@material-ui/core";
 import Box from "@material-ui/core/Box";
 import { shell } from "electron";
 import React, { FC } from "react";
 import { useTranslation } from "react-i18next";
-import { FaGlobeAmericas, FaInfo, FaQuestionCircle } from "react-icons/fa";
+import {
+  FaEnvelope,
+  FaGlobeAmericas,
+  FaGrinStars,
+  FaInfo,
+  FaQuestionCircle,
+} from "react-icons/fa";
 import { versionName } from "version";
 import packageJson from "../../../../package.json";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import { Theme } from "@material-ui/core/styles/createMuiTheme";
+
+const useLocalStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    icon: {
+      paddingRight: theme.spacing(1),
+    },
+  })
+);
+
+const aboutItems = [
+  {
+    link: `${ARCHIFILTRE_SITE_URL}/produit#${packageJson.version.replace(
+      /\./g,
+      ""
+    )}`,
+    Icon: FaGlobeAmericas,
+    label: "settingsModal.website",
+  },
+  {
+    link: "https://github.com/SocialGouv/archifiltre/wiki/Wiki-Archifiltre",
+    Icon: FaQuestionCircle,
+    label: "settingsModal.wiki",
+  },
+  {
+    link: "https://bit.ly/315pAd8",
+    Icon: FaGrinStars,
+    label: "folderDropzone.feedback",
+  },
+  {
+    link: "mailto:archifiltre@sg.social.gouv.fr",
+    Icon: FaEnvelope,
+    label: "folderDropzone.contactUs",
+  },
+];
 
 const About: FC = () => {
   const { t } = useTranslation();
+  const classes = useLocalStyles();
 
-  const onSiteClick = (event) => {
+  const onClick = (event, link) => {
     event.preventDefault();
-    shell.openExternal(
-      `${ARCHIFILTRE_SITE_URL}/produit#${packageJson.version.replace(/\./g, "")}`
-    );
-  };
-
-  const onWikiClick = (event) => {
-    event.preventDefault();
-    shell.openExternal(
-      "https://github.com/SocialGouv/archifiltre/wiki/Wiki-Archifiltre"
-    );
+    shell.openExternal(link);
   };
 
   return (
     <Box paddingTop={1}>
       <Box display="flex" pb={1}>
-        <FaInfo />
-        &nbsp;{`v${packageJson.version} ${versionName}`}
+        <FaInfo className={classes.icon} />
+        {`v${packageJson.version} ${versionName}`}
       </Box>
-      <Box pb={1}>
-        <Link component="button" variant="h4" onClick={onSiteClick}>
-          <FaGlobeAmericas />
-          &nbsp;{t("settingsModal.website")}
-        </Link>
-      </Box>
-      <Box>
-        <Link component="button" variant="h4" onClick={onWikiClick}>
-          <FaQuestionCircle />
-          &nbsp;{t("settingsModal.wiki")}
-        </Link>
-      </Box>
+      {aboutItems.map(({ link, Icon, label }) => (
+        <Box pb={1} key={label}>
+          <Link
+            component="button"
+            variant="h4"
+            onClick={(event) => onClick(event, link)}
+          >
+            <Icon className={classes.icon} />
+            {t(label)}
+          </Link>
+        </Box>
+      ))}
     </Box>
   );
 };

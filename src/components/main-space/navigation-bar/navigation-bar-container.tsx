6c94d3a260de71bd7ae01aa211651a9d382958d8
Benjamin Morali <benjaminmorali4@hotmail.fr>
2021-02-24 15:00:33 +0100

    fix(zoom): fix double click zoom (#1205)

--- src/components/main-space/navigation-bar/navigation-bar-container.tsx
+++ src/components/main-space/navigation-bar/navigation-bar-container.tsx
@@ -1,6 +1,5 @@
 import React, { useCallback } from "react";
 import { useDispatch } from "react-redux";
-import { setLockedElementId } from "reducers/workspace-metadata/workspace-metadata-actions";
 import { NavigationBar } from "./navigation-bar";
 import {
   ElementWeightMethod,
@@ -9,8 +8,6 @@ import {
 } from "reducers/icicle-sort-method/icicle-sort-method-types";
 import { setIcicleSortMethodThunk } from "reducers/icicle-sort-method/icicle-sort-method-thunk";
 import { useIcicleSortMethod } from "reducers/icicle-sort-method/icicle-sort-method-selectors";
-import { useIsZoomed } from "reducers/main-space-selection/main-space-selection-selectors";
-import { resetZoom } from "reducers/main-space-selection/main-space-selection-action";
 import {
   setElementWeightMethod,
   setIcicleColorMode,
@@ -30,16 +27,6 @@ const NavigationBarContainer = () => {
     [dispatch]
   );
 
-  const isZoomed = useIsZoomed();
-
-  const setNoFocus = useCallback(() => dispatch(setLockedElementId("")), [
-    dispatch,
-  ]);
-
-  const resetZoomCallback = useCallback(() => dispatch(resetZoom()), [
-    dispatch,
-  ]);
-
   const setElementWeightMethodCallback = useCallback(
     (method: ElementWeightMethod) => dispatch(setElementWeightMethod(method)),
     [dispatch]
@@ -54,11 +41,8 @@ const NavigationBarContainer = () => {
     <NavigationBar
       elementWeightMethod={elementWeightMethod}
       icicleColorMode={icicleColorMode}
-      isZoomed={isZoomed}
       icicleSortMethod={icicleSortMethod}
       setIcicleSortMethod={setIcicleSortMethodCallback}
-      setNoFocus={setNoFocus}
-      resetZoom={resetZoomCallback}
       setElementWeightMethod={setElementWeightMethodCallback}
       setIcicleColorMode={setIcicleColorModeCallback}
     />

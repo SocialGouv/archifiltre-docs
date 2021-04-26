65fe8011e2663d74fa8d39934ae76ad4421f36c2
Benjamin Morali <benjaminmorali4@hotmail.fr>
2021-04-26 15:05:15 +0200

    refactor(picker): refactor navigation bar pickers (#1261)

--- src/components/main-space/navigation-bar/icicle-color-mode-picker.tsx
+++ src/components/main-space/navigation-bar/icicle-color-mode-picker.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react";
 import { IcicleColorMode } from "reducers/icicle-sort-method/icicle-sort-method-types";
 import { useTranslation } from "react-i18next";
 import OptionsPicker from "./options-picker";
+import { FaPalette } from "react-icons/fa";
 
 type IcicleColorModePickerProps = {
   icicleColorMode: IcicleColorMode;
@@ -31,6 +32,7 @@ const IcicleColorModePicker: FC<IcicleColorModePickerProps> = ({
       value={icicleColorMode}
       setValue={setIcicleColorMode}
       options={options}
+      icon={<FaPalette />}
     />
   );
 };

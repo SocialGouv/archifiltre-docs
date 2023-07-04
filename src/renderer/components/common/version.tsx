c97fec989dc7ac4f2d5fd3bf11e29ae88e7a5fd5
mehdilouraoui <37937348+mehdilouraoui@users.noreply.github.com>
2023-07-04 16:23:51 +0200

    feat(version): add app version in main views (#1540)

---
+++ src/renderer/components/common/version.tsx
@@ -0,0 +1,17 @@
+import type { CSSProperties } from "react";
+import React from "react";
+
+import { version, versionName } from "../../version";
+
+export const Version: React.FC = () => {
+  const style: CSSProperties = {
+    fontSize: "10px",
+    margin: "0 0 10px",
+    textAlign: "center",
+  };
+  return (
+    <div className="version" style={style}>
+      {`v${version} ${versionName}`}
+    </div>
+  );
+};

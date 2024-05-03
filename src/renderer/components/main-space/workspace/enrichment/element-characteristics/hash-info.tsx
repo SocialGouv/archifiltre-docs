858633f8f36fd959c87a8ac5bbd1578bd17fd007
Timothée Malard <timothee.malard.pro@gmail.com>
2024-05-03 15:52:00 +0200

    feat: improve waiting hash message

--- src/renderer/components/main-space/workspace/enrichment/element-characteristics/hash-info.tsx
+++ src/renderer/components/main-space/workspace/enrichment/element-characteristics/hash-info.tsx
@@ -2,9 +2,10 @@ import { Hashicon as HashIcon } from "@emeraldpay/hashicon-react/lib/component";
 import Tooltip from "@material-ui/core/Tooltip";
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { FaAtom } from "react-icons/fa";
 
 export interface HashInfoProps {
-  hash: string;
+  hash?: string;
 }
 
 export const HashInfo: React.FC<HashInfoProps> = ({ hash }) => {
@@ -13,11 +14,10 @@ export const HashInfo: React.FC<HashInfoProps> = ({ hash }) => {
 
   return (
     <span>
-      <span>{hash}</span>&nbsp;
+      <span>{hash || t("report.hashCalculateInProgress")}</span>&nbsp;
       <Tooltip title={hashIconExplanation}>
         <span>
-          {/* TODO: use simple icon */}
-          <HashIcon value={hash} size={12} />
+          {hash ? <HashIcon value={hash} size={12} /> : <FaAtom size={12} />}
         </span>
       </Tooltip>
     </span>

4e67168485699ebcf171ac6cc15c692ac6b70969
Timothée Malard <timothee.malard.pro@gmail.com>
2024-06-04 15:20:01 +0200

    fix: posthog

--- src/renderer/constants.ts
+++ src/renderer/constants.ts
@@ -1,6 +1,5 @@
 export const ARCHIFILTRE_CONTACT_EMAIL = "archifiltre@fabrique.social.gouv.fr";
-export const FEEDBACK_LINK =
-  "https://archifiltre.fabrique.social.gouv.fr/questionnaire/";
+export const FEEDBACK_LINK = "https://form.typeform.com/to/lTwzr0dP";
 export const CONTACT_LINK = `mailto:${ARCHIFILTRE_CONTACT_EMAIL}`;
 export const DOCUMENTATION_LINK =
   "https://github.com/SocialGouv/archifiltre-docs/wiki/Wiki-Archifiltre";
--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -7,6 +7,8 @@ require("dotenv").config();
 
 module.exports =
   /** @param {import("webpack").Configuration} config */ function (config) {
+    const isProd = config.mode === "production";
+
     const workers = glob
       .sync("./src/renderer/**/*.fork.ts")
       .map((filePath) => {
@@ -21,32 +23,8 @@ module.exports =
         acc[path.join(directoryPath, name)] = filePath;
         return acc;
       }, {});
-    // const styleRules = config.module.rules.filter((rule) =>
-    //   rule.test.toString().match(/css|less|s\(\[ac\]\)ss/)
-    // );
 
-    // styleRules.forEach((rule) => {
-    //   const uses = rule.use;
-    //   if (!Array.isArray(uses)) {
-    //     return;
-    //   }
-
-    //   const cssLoader = uses.find((use) => use.loader === "css-loader");
-    //   if (typeof cssLoader === "object") {
-    //     cssLoader.options = {
-    //       ...cssLoader.options,
-    //       esModule: true,
-    //       localsConvention: "camelCase",
-    //       modules: {
-    //         localIdentName: "[local]___[hash:base64:5]",
-    //         mode: "local",
-    //       },
-    //       sourceMap: true,
-    //     };
-    //   }
-    // });
-
-    if (config.mode === "production") {
+    if (isProd) {
       if (!config.plugins?.length) {
         config.plugins = [];
       }
